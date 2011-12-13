247d0202fdf53c98cab5a2ce333d25ba5f8a60bb
David Laribee <laribee@gmail.com>
2011-12-13 14:37:21 -0500

    before first compile

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,22 +1,60 @@
 using Bowling.Specs.Infrastructure;
 
+
 namespace specs_for_bowling
 {
-    public class when_everything_is_wired_up : concerns
-    {
-        private bool _itWorked;
 
+    public class BowlingGame
+    {
+        private int _current_roll_number = 0;
+        private int _current_score = 0;
+
+        public BowlingGame()
+        {
+        }
+
+        public int getScore()
+        {
+            return _current_score;
+        }
+
+        public void Roll(int pins_fallen_)
+        {
+            // magic.
+        }
+    }
+
+    public class when_20_gutter_balls_are_rolled : concerns
+    {
+        private BowlingGame _game;
         protected override void context()
         {
-            _itWorked = true;
+            _game = new BowlingGame();
+            20.times( () => _game.Roll(0) );
         }
 
         [Specification]
-        public void it_works()
+        public void the_score_is_0()
         {
-            _itWorked.should_be_true("we're ready to roll!");
-            
+            _game.getScore().should_equal(0)
         }
 
     }
 }
+
+
+
+
+// exception GameOver
+//  var sc = new ScoreKeeper() ;
+//  assert sc.getCurrentScore == 0;
+// shouldRaiseException(...)
+// foreach( i in 0..20) {
+    // sc.nextRoll(0) 
+    // }   
+//assert sc.getScore() == 0;
+//
+//
+// 
+//
+//
