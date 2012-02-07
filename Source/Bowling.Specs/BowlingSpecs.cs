fb9e919afea29572674f318916dfb365102300f8
Dan Lash <danlash@gmail.com>
2012-02-07 14:01:06 -0500

    open frames pass specs

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,20 +1,44 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
 
 namespace specs_for_bowling
 {
-	public class when_everything_is_wired_up : concerns
+	public class when_rolling_all_gutter_balls : concerns
 	{
-		private bool _itWorked;
+		private BowlingGame _game;
 
 		protected override void context()
 		{
-			_itWorked = true;
+			_game = new BowlingGame();
+			20.times(() => _game.Roll(0));
 		}
 
 		[Specification]
-		public void it_works()
+		public void score_is_zero()
 		{
-			_itWorked.should_be_true("we're ready to roll!");
+			_game.Score().should_equal(0);
+		}
+	}
+
+	public class when_rolling_all_open_frames : concerns
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = new BowlingGame();
+			_game.Roll(3);
+			_game.Roll(2);
+			_game.Roll(1);
+			_game.Roll(3); //2 frames worth
+
+			16.times(() => _game.Roll(0));
+		}
+
+		[Specification]
+		public void score_is_9()
+		{
+			_game.Score().should_equal(9);
 		}
 	}
 }
---
+++ Source/Bowling/BowlingGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class BowlingGame
+	{
+		private int _score;
+
+		public void Roll(int pinsKnockedDown)
+		{
+			_score += pinsKnockedDown;
+		}
+
+		public int Score()
+		{
+			return _score;
+		}
+	}
+}
