65eb112ef7b0b314608aae3a91c875e618174066
Jesse Riley <jeriley@gmail.com>
2012-07-02 15:11:17 -0400

    single strike works

--- Source/Bowling/BowlingGame.cs
+++ Source/Bowling/BowlingGame.cs
@@ -10,7 +10,7 @@ namespace Bowling
 		private const int STRIKE = 10;
 		private int _score = 0;
 		private int _numberOfRollsInCurrentFrame = 0;
-		private int _numberOfFramesBowled = 1;
+		private int _numberOfFramesBowled = 0;
 		private int _previousFrameScore = 0;
 		private bool _areInStrikeMode = false;
 		private int _numberOfRollsAfterStrike = 0;
@@ -35,27 +35,30 @@ namespace Bowling
 				_numberOfRollsAfterStrike = 0;
 			}
 
-			if (isNewFrame(knockedDownPins)) //first roll or they got a strike
+			if (isNewFrame()) //first roll or they got a strike
 			{
-				//set to strike mode to track the next to rolls
-				if (knockedDownPins == STRIKE)
-					_areInStrikeMode = true;
-
 				_numberOfFramesBowled++;
 
+				//set to strike mode to track the next to rolls, and reset rolls
+				if (knockedDownPins == STRIKE)
+				{
+					_areInStrikeMode = true;
+					_numberOfRollsInCurrentFrame = 0;
+				}
+
 				//apply bonus if deserving?
-				if (_previousFrameScore == 10)
+				if (_previousFrameScore == 10 && !_areInStrikeMode)
 				{
 					_score += knockedDownPins;
 				}
-				
+
 				//reset previous frame
 				_previousFrameScore = knockedDownPins;
-				_numberOfRollsInCurrentFrame = 0;
 			}
 			else //second roll
 			{
 				_previousFrameScore += knockedDownPins;
+				_numberOfRollsInCurrentFrame = 0;
 			}
 		}
 
@@ -63,13 +66,13 @@ namespace Bowling
 
 		public bool IsGameComplete
 		{
-			get { return _numberOfFramesBowled > 10; }
+			get { return _numberOfFramesBowled >= 10; }
 		}
 
-		private bool isNewFrame(int knockedDownPins)
+		private bool isNewFrame()
 		{
 			//current frame they have bowled two times or it was a strike
-			return (_numberOfRollsInCurrentFrame >= 2 || knockedDownPins == STRIKE);
+			return (_numberOfRollsInCurrentFrame == 1);
 		}
 	}
 }
