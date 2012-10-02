56468001e80d3080a8424b487c374fc64c1d736e
Cory Wheeler <cory.wheeler@versionone.com>
2012-10-02 13:58:53 -0400

    rolling all gutter balls gives zero

---
+++ Source/Bowling.Specs/BowlingGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling.Specs
+{
+	public class BowlingGame
+	{
+		public int Score { get; private set; }
+	}
+}
--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,20 +1,34 @@
 using Bowling.Specs.Infrastructure;
+using Bowling.Specs;
+
+/*
+ * 
+when rolling all gutter balls, the score is 0.
+when the first frame is a spare and the rest score 2, the score is 48.
+when the first 2 frames are spare and the rest score 2, the score is 56.
+when 10 frames have been bowled, don't allow any more to be bowled.
+when the first frame is a strike and the rest score 2, the score is 50.
+when the first 2 frames are strikes and the rest score 2, the score is 68.
+when rolling a perfect game, the score is 300.
+when rolling alternate strikes and spares, the score is 200.
+ * 
+ */
 
 namespace specs_for_bowling
 {
-	public class when_everything_is_wired_up : concerns
+	public class when_rolling_all_gutter_balls : concerns
 	{
-		private bool _itWorked;
+		private BowlingGame SUT;
 
 		protected override void context()
 		{
-			_itWorked = true;
+			SUT = new BowlingGame();
 		}
 
 		[Specification]
-		public void it_works()
+		public void score_is_zero()
 		{
-			_itWorked.should_be_true("we're ready to roll!");
+			SUT.Score.should_equal(0);
 		}
 	}
 }
