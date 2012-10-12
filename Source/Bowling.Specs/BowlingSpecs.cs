fc246caa8916d12bcb0afc71a7ef87a879a59f6e
Kris Molter <kris.molter@versionone.com>
2012-10-12 13:07:48 -0400

    initial crappy code implementation

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,4 +1,5 @@
 using Bowling.Specs.Infrastructure;
+using Bowling;
 
 namespace specs_for_bowling
 {
@@ -15,6 +16,165 @@ namespace specs_for_bowling
 		public void it_works()
 		{
 			_itWorked.should_be_true("we're ready to roll!");
+		}
+	}
+
+	public class when_rolling_all_gutter_balls : concerns
+	{
+
+		private Game _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new Game();
+		}
+
+		[Specification]
+		public void score_is_zero()
+		{
+			20.times(() => _bowlingGame.Roll(0)); //roll ten frames, miss everything
+			_bowlingGame.Score.should_equal(0);
+		}
+	}
+
+	public class when_the_first_frame_is_a_spare_thens_2s : concerns
+	{
+		private Game _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new Game();
+		}
+
+		[Specification]
+		public void score_is_48()
+		{
+			2.times(() => _bowlingGame.Roll(5)); //spare
+			18.times(() => _bowlingGame.Roll(2));
+			_bowlingGame.Score.should_equal(48);
+		}
+	}
+
+	public class when_the_first_two_frames_are_spares_then_2s : concerns
+	{
+		private Game _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new Game();
+		}
+
+		[Specification]
+		public void score_is_56()
+		{
+			_bowlingGame.Roll(2); //spares
+			_bowlingGame.Roll(8); //spares
+			_bowlingGame.Roll(2); //spares
+			_bowlingGame.Roll(8); //spares
+			16.times(() => _bowlingGame.Roll(2));
+			_bowlingGame.Score.should_equal(56);
+		}
+	}
+
+	public class when_the_ten_frames_bowled : concerns
+	{
+		private Game _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new Game();
+		}
+
+		[Specification]
+		public void the_game_is_over()
+		{
+			_bowlingGame.Roll(2); //spares
+			_bowlingGame.Roll(8); //spares
+			_bowlingGame.Roll(2); //spares
+			_bowlingGame.Roll(8); //spares
+			16.times(() => _bowlingGame.Roll(2));
+			_bowlingGame.Score.should_equal(56);
+			_bowlingGame.IsGameOver.should_equal(true);
+		}
+	}
+
+	public class when_the_first_frame_strike_then_2s : concerns
+	{
+		private Game _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new Game();
+		}
+
+		[Specification]
+		public void the_score_is_50()
+		{
+			_bowlingGame.Roll(10); //strike
+			18.times(() => _bowlingGame.Roll(2));
+			_bowlingGame.Score.should_equal(50);
+		}
+	}
+
+	public class when_the_first_two_frames_are_strike_then_2s : concerns
+	{
+		private Game _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new Game();
+		}
+
+		[Specification]
+		public void the_score_is_68()
+		{
+			2.times(() => _bowlingGame.Roll(10)); //strikes
+			16.times(() => _bowlingGame.Roll(2));
+			_bowlingGame.Score.should_equal(68);
+		}
+	}
+
+	public class when_perfect_game : concerns
+	{
+		private Game _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new Game();
+		}
+
+		[Specification]
+		public void the_score_is_300()
+		{
+			9.times(() => _bowlingGame.Roll(10)); //strikes
+			3.times(() => _bowlingGame.Roll(10)); //tenth frame
+			_bowlingGame.Score.should_equal(300);
+		}
+	}
+
+	public class when_alternating_strikes_and_spares : concerns
+	{
+		private Game _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new Game();
+		}
+
+		[Specification]
+		public void the_score_is_200()
+		{
+			4.times(() =>
+				        {
+					        _bowlingGame.Roll(10);
+					        _bowlingGame.Roll(2);
+					        _bowlingGame.Roll(8);
+				        }); //alt strikes and spares
+			_bowlingGame.Roll(10); //ninth
+			_bowlingGame.Roll(2); //tenth
+			_bowlingGame.Roll(8);
+			_bowlingGame.Roll(10); //tenth
+			_bowlingGame.Score.should_equal(200);
 		}
 	}
 }
---
+++ Source/Bowling/Game.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class Game
+	{
+		private List<List<int>> _frames = new List<List<int>>();
+		private int _currentFrameIndex = 0;
+		private int _currentRoll = 1;
+		private int _score = 0;
+
+		public Game()
+		{
+			_frames.Add(new List<int>());
+		}
+
+		public int Score
+		{
+			get
+			{
+				var score = 0;
+				_frames.ForEach(frame => score += frame.Sum());
+				return score;
+			}
+		}
+
+		public bool IsGameOver
+		{
+			get { return _frames.Count == 10; }
+		}
+
+		public void Roll(int pins)
+		{
+			_score += pins;
+			_frames[_currentFrameIndex].Add(pins);
+
+			if (_currentRoll == 1 && _currentFrameIndex > 0)
+			{
+				if (_frames[_currentFrameIndex - 1][0] == 10 || 
+					((_frames[_currentFrameIndex - 1][0] + _frames[_currentFrameIndex - 1][1]) == 10)
+					)
+				{
+					_frames[_currentFrameIndex - 1].Add(pins);
+					if (_frames[_currentFrameIndex - 1][0] == 10)
+						if ((_currentFrameIndex - 2) >= 0)
+						{
+							if (_frames[_currentFrameIndex - 2][0] == 10)
+								_frames[_currentFrameIndex - 2].Add(pins);
+						}
+				}
+			}
+			else if (_currentRoll == 2 && _currentFrameIndex > 0)
+			{
+				if (_frames[_currentFrameIndex - 1][0] == 10)
+				{
+					if ((_currentFrameIndex - 2) >= 0 && _frames.Count == 10 && pins == 10)
+						if (_frames[_currentFrameIndex - 2].Sum() < 30 && _frames[_currentFrameIndex - 1][0] == 10)
+							_frames[_currentFrameIndex - 2].Add(pins);
+					if (_frames[_currentFrameIndex - 1].Sum() < 30)
+						_frames[_currentFrameIndex - 1].Add(pins);
+				}
+			}
+			
+			if ((_currentRoll == 2 || pins == 10) && _frames.Count != 10) //max number of valid rolls for each frame
+			{
+				_score = 0;
+				_currentRoll = 1;
+				if (!IsGameOver)
+					_frames.Add(new List<int>());
+				_currentFrameIndex = _frames.Count - 1;
+			}
+			else
+				_currentRoll++;
+		}
+	}
+}
