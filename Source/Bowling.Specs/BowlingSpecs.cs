4d96434c7c138c2ec1408c698591cff06c751475
Dan Lash <danlash@gmail.com>
2013-02-18 13:53:17 -0500

    gutters and open frames

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,20 +1,41 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
 
 namespace specs_for_bowling
 {
-	public class when_everything_is_wired_up : concerns
+	internal class when_rolling_all_gutters : concerns
 	{
-		private bool _itWorked;
+		private Game _game;
 
 		protected override void context()
 		{
-			_itWorked = true;
+			_game = new Game();
+			
+			20.times(() => _game.Roll(0));
 		}
 
 		[Specification]
-		public void it_works()
+		private void the_score_is_zero()
 		{
-			_itWorked.should_be_true("we're ready to roll!");
+			_game.Score().should_equal(0);
+		}
+	}
+
+	internal class when_rolling_all_open_threes : concerns
+	{
+		private Game _game;
+
+		protected override void context()
+		{
+			_game = new Game();
+
+			20.times(() => _game.Roll(3));
+		}
+
+		[Specification]
+		private void the_score_is_sixty()
+		{
+			_game.Score().should_equal(60);
 		}
 	}
 }
---
+++ Source/Bowling/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class Game
+	{
+		private int _score;
+
+		public void Roll(int number)
+		{
+			_score += number;
+		}
+
+		public int Score()
+		{
+			return _score;
+		}
+	}
+}
