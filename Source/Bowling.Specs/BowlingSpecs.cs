2ad3b9b69735b8c28d38745aa810e9da031be182
Dan Lash <danlash@gmail.com>
2013-02-18 14:48:47 -0500

    one strike

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,3 +1,4 @@
+using System;
 using Bowling;
 using Bowling.Specs.Infrastructure;
 
@@ -58,6 +59,66 @@ namespace specs_for_bowling
 		private void the_score_is_sixty()
 		{
 			_game.Score().should_equal(67);
+		}
+	}
+
+	internal class when_you_roll_two_spares_then_all_threes : concerns
+	{
+		private Game _game;
+
+		protected override void context()
+		{
+			_game = new Game();
+
+			_game.Roll(4);
+			_game.Roll(6); //spare (10 ... + 7) 17
+			_game.Roll(7);
+			_game.Roll(3); //spare (17 + 10 + 3) 30
+
+			16.times(() => _game.Roll(3)); 
+		}
+
+		[Specification]
+		private void the_score_is_seventy_eight()
+		{
+			_game.Score().should_equal(78);
+		}
+	}
+
+	internal class when_you_roll_a_complete_game : concerns
+	{
+		private Game _game;
+
+		protected override void context()
+		{
+			_game = new Game();
+
+			20.times(() => _game.Roll(3)); 
+		}
+
+		[Specification]
+		private void rolling_again_fails()
+		{
+			typeof (GameCompleteException).should_be_thrown_by(() => _game.Roll(3));
+		}
+	}
+
+	internal class when_you_roll_a_strike_and_then_twos : concerns
+	{
+		private Game _game;
+
+		protected override void context()
+		{
+			_game = new Game();
+
+			_game.Roll(10);
+			18.times(() => _game.Roll(3));
+		}
+
+		[Specification]
+		private void scores_a_seventy()
+		{
+			_game.Score().should_equal(70);
 		}
 	}
 
--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -10,21 +9,53 @@ namespace Bowling
 		private int _score;
 
 		private int _roll_count = 0;
+		private int _frame_count = 0;
 
-		private int[] _rolls = new int[21];
+		private readonly int[][] _rolls = new int[10][];
 
 		public void Roll(int number)
 		{
-			_roll_count ++;
-			_score += number;
-			_rolls[_roll_count] = number;
+			if (_frame_count > 9) throw new GameCompleteException();
 
-			if ((_roll_count > 2) && 
-				(_rolls[_roll_count - 1] + _rolls[_roll_count - 2] == 10) &&
-				(_roll_count % 2 == 1))
+			_score += number;
+
+			if(_roll_count == 0)
+				_rolls[_frame_count] = new int[2]; //initialize frame
+
+			var current_frame = _rolls[_frame_count];
+			current_frame[_roll_count] = number;
+
+			if (_frame_count > 0) //bonus check 
 			{
-				_score += number;
+				var previousFrame = _rolls[_frame_count - 1];
+				if (_roll_count == 1 && previousFrame[0] == 10) //strike
+				{
+					_score += current_frame[0] + current_frame[1];
+				}
+				else if (_roll_count == 0 && 
+					previousFrame[0] != 10 &&
+					previousFrame[0] + previousFrame[1] == 10) //spare 
+				{
+					_score += number;
+				}
 			}
+
+
+			if (_roll_count == 1) //end of frame
+			{
+				_frame_count ++;
+				_roll_count = 0;
+			} 
+			else if (_roll_count == 0 && number == 10) //end of strike
+			{
+				_frame_count++;
+				_roll_count = 0;
+			}
+			else //middle of the frame
+			{
+				_roll_count ++;
+			}
+
 		}
 
 		public int Score()
