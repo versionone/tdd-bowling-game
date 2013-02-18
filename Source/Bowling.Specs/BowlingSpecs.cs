142db511525f83c0c0bc8d0ff4a6884426e61460
Dan Lash <danlash@gmail.com>
2013-02-18 14:04:36 -0500

    first spare with open frames

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -38,4 +38,27 @@ namespace specs_for_bowling
 			_game.Score().should_equal(60);
 		}
 	}
+
+
+	internal class when_you_roll_a_spare_then_all_threes : concerns
+	{
+		private Game _game;
+
+		protected override void context()
+		{
+			_game = new Game();
+
+			_game.Roll(4);
+			_game.Roll(6); //spare (10 ... + 3) 13
+
+			18.times(() => _game.Roll(3)); //54+13 = 67
+		}
+
+		[Specification]
+		private void the_score_is_sixty()
+		{
+			_game.Score().should_equal(67);
+		}
+	}
+
 }
--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -9,9 +9,22 @@ namespace Bowling
 	{
 		private int _score;
 
+		private int _roll_count = 0;
+
+		private int[] _rolls = new int[21];
+
 		public void Roll(int number)
 		{
+			_roll_count ++;
 			_score += number;
+			_rolls[_roll_count] = number;
+
+			if ((_roll_count > 2) && 
+				(_rolls[_roll_count - 1] + _rolls[_roll_count - 2] == 10) &&
+				(_roll_count % 2 == 1))
+			{
+				_score += number;
+			}
 		}
 
 		public int Score()
