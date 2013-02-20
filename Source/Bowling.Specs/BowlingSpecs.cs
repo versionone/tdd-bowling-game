ee97e6149dd6b12b8dc20e364add6eedccf29bc6
codeEmitter <greg.johnson@versionone.com>
2013-02-20 14:31:10 -0500

    Scoring all zeros and empty frames completed.

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,20 +1,42 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
 
 namespace specs_for_bowling
 {
-	public class when_everything_is_wired_up : concerns
+	public class when_rolling_all_gutters : concerns
 	{
-		private bool _itWorked;
+	    private int _score;
 
-		protected override void context()
+	    protected override void context()
 		{
-			_itWorked = true;
+		    var game = new Game();
+            20.times(()=> game.Roll(0));
+
+		    _score = game.Score();
 		}
 
 		[Specification]
-		public void it_works()
+		public void the_score_is_zero()
 		{
-			_itWorked.should_be_true("we're ready to roll!");
+		    _score.should_equal(0);
 		}
 	}
+
+    public class when_rolling_open_frame : concerns
+    {
+        private int _score;
+
+        protected override void context()
+        {
+            var game = new Game();
+            20.times(() => game.Roll(2));
+            _score = game.Score();
+        }
+
+        [Specification]
+        public void the_score_is_zero()
+        {
+            _score.should_equal(40);
+        }
+    }
 }
---
+++ Source/Bowling/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+    public class Game
+    {
+        private int _score;
+
+        public Game()
+        {
+            _score = 0;
+        }
+
+        public void Roll(int _pins)
+        {
+            if (_pins == 0) _score = 0;
+            _score += _pins;
+        }
+
+        public int Score()
+        {
+            return _score;
+        }
+    }
+}
