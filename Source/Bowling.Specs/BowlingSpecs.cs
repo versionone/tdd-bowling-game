4a4c83a13782ee5e0878e3a7f5a419d9bfd7a99f
James Holmes <jholmes@JHolmes.corp.versionone.net>
2013-03-13 16:51:06 -0400

    added Frame class

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,4 +1,6 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
+using specs_for_bowling;
 
 namespace specs_for_bowling
 {
@@ -50,7 +52,7 @@ namespace specs_for_bowling
 		}
 	}
 
-	public class when_rolling_a_spare_followed_by_2s : concerns
+	public class when_rolling_a_spare_followed_by_all_2s : concerns
 	{
 		private readonly Game _game = new Game();
 
@@ -66,6 +68,96 @@ namespace specs_for_bowling
 		public void the_score_is_48()
 		{
 			_game.Score().should_equal(48);
+		}
+	}
+
+	public class when_rolling_two_balls_spanning_two_frames_that_would_have_equaled_a_spare : concerns
+	{
+		private readonly Game _game = new Game();
+
+		protected override void context()
+		{
+			_game.Roll(1);
+			_game.Roll(8);
+
+			_game.Roll(2);
+			_game.Roll(2);
+
+			16.times(() => _game.Roll(0));
+		}
+
+		[Specification]
+		public void the_score_is_13()
+		{
+			_game.Score().should_equal(13);
+		}
+	}
+
+	public class when_rolling_two_spares_followed_by_all_2s : concerns
+	{
+		private readonly Game _game = new Game();
+
+		protected override void context()
+		{
+			_game.Roll(8);
+			_game.Roll(2);
+
+			18.times(() => _game.Roll(2));
+		}
+
+		[Specification]
+		public void the_score_is_48()
+		{
+			//_game.Score().should_equal(48);
+		}
+	}
+}
+
+namespace specs_for_frames
+{
+	public class when_the_sum_of_firstRoll_and_secondRoll_equals_ten_ : concerns
+	{
+		private readonly Frame _frame = new Frame();
+
+		protected override void context()
+		{
+			_frame.AddPins(8);
+			_frame.AddPins(2);
+		}
+
+		[Specification]
+		public void frame_is_spare()
+		{
+			_frame.IsSpare().should_equal(true);
+		}
+
+		[Specification]
+		public void frame_is_not_open()
+		{
+			_frame.IsOpen().should_equal(false);
+		}
+	}
+
+	public class when_the_sum_of_firstRoll_and_secondRoll_is_less_than_ten_ : concerns
+	{
+		private readonly Frame _frame = new Frame();
+
+		protected override void context()
+		{
+			_frame.AddPins(8);
+			_frame.AddPins(1);
+		}
+
+		[Specification]
+		public void frame_is_open()
+		{
+			_frame.IsOpen().should_equal(true);
+		}
+
+		[Specification]
+		public void frame_is_not_spare()
+		{
+			_frame.IsSpare().should_equal(false);
 		}
 	}
 }
--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -1,32 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using Bowling;
+
 namespace specs_for_bowling
 {
 	public class Game
 	{
-		private int _score = 0;
-		private int _currentRoll = 0;
+		private readonly List<Frame> _frames = new List<Frame>();
 		private readonly int[] _rolls = new int[21];
+
+		public Game()
+		{
+			_frames.Add(new Frame());
+		}
 
 		public void Roll(int pins)
 		{
-			_rolls[_currentRoll++] = pins;
+			Frame frame = _frames.Last();
+			if (frame.IsFrameComplete())
+			{
+				frame = new Frame();
+				_frames.Add(frame);
+			}
+
+			frame.AddPins(pins);
 		}
 
 		public int Score()
 		{
-			for (int i = 0; i < _currentRoll; i++)
+			int score = 0;
+
+			for (int index = 0; index < _frames.Count; index++)
 			{
-				if (_rolls[i] + _rolls[i + 1] == 10)
+				var frame = _frames[index];
+				if (frame.IsSpare())
 				{
-					_score += 10 + _rolls[i + 2];
-					i += 1;
+					score += (frame.FirstRoll.Value + frame.SecondRoll.Value);
+					Frame nextFrame = _frames[index + 1];
+					score += nextFrame.FirstRoll.Value;
 				}
 				else
 				{
-					_score += _rolls[i];
+					score += (frame.FirstRoll.Value + frame.SecondRoll.Value);
 				}
 			}
 
-			return _score;
+			return score;
 		}
 	}
 }
