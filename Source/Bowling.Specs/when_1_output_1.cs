97e7b2bd2e04b5590b5e63f3d0e15286be13c4af
James Holmes <32bitkid@gmail.com>
2013-11-21 13:26:45 -0500

    working fizzbuzz

---
+++ Source/Bowling.Specs/when_1_output_1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bowling.Specs.Infrastructure;
+
+namespace Bowling.Specs
+{
+	class when_1 : concerns<FizzBuzz>
+	{
+		private string _value;
+
+		protected override void context()
+		{
+			_value = build_up().GetResult(1);
+		}
+
+		[Specification]
+		public void result_is_1()
+		{
+			_value.should_equal("1");
+		}
+	}
+
+	class when_6 : concerns<FizzBuzz>
+	{
+		private string _value;
+
+		protected override void context()
+		{
+			_value = build_up().GetResult(6);
+		}
+
+		[Specification]
+		public void result_is_fizz()
+		{
+			_value.should_equal("fizz");
+		}
+	}
+
+	class when_10 : concerns<FizzBuzz>
+	{
+		private string _value;
+
+		protected override void context()
+		{
+			_value = build_up().GetResult(10);
+		}
+
+		[Specification]
+		public void result_is_buzz()
+		{
+			_value.should_equal("buzz");
+		}
+	}
+
+	class when_15 : concerns<FizzBuzz>
+	{
+		private string _value;
+
+		protected override void context()
+		{
+			_value = build_up().GetResult(15);
+		}
+
+		[Specification]
+		public void result_is_fizzbuzz()
+		{
+			_value.should_equal("fizzbuzz");
+		}
+	}
+}
---
+++ Source/Bowling/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class FizzBuzz
+	{
+		public string GetResult(int value)
+		{
+			string returnVal = value.ToString();
+			if (value % 3 == 0 && value % 5 != 0)
+				returnVal = "fizz";
+			if (value % 5 == 0 && value % 3 != 0)
+				returnVal = "buzz";
+			if (value % 5 == 0 && value % 3 == 0)
+				returnVal = "fizzbuzz";
+			return returnVal;
+		}
+	}
+}
