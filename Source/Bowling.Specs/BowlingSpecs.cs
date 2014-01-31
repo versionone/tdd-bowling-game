f85f634422e0ac199868cc134dcfdff00a41b3c2
Matt Higgins <mhhiggins@gmail.com>
2014-01-31 13:21:48 -0500

    scoring "regular" rolls

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,3 +1,4 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
 
 namespace specs_for_bowling
@@ -15,6 +16,56 @@ namespace specs_for_bowling
 		public void it_works()
 		{
 			_itWorked.should_be_true("we're ready to roll!");
+		}
+	}
+
+	public class when_rolling_all_gutter_balls : concerns
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = new BowlingGame();
+		}
+
+		[Specification]
+		public void the_score_is_zero()
+		{
+			_game.Score.should_equal(0);
+		}
+	}
+
+	public class when_rolling_twos : concerns
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = new BowlingGame();
+			20.times(() => _game.Roll(2));
+		}
+
+		[Specification]
+		public void the_score_is_for_twos()
+		{
+			_game.Score.should_equal(40);
+		}
+	}
+
+	public class when_rolling_2s_and_5s : concerns
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = new BowlingGame();
+			10.times(() => { _game.Roll(2); _game.Roll(5); });
+		}
+
+		[Specification]
+		public void the_score_is_70()
+		{
+			_game.Score.should_equal(70);
 		}
 	}
 }
---
+++ Source/Bowling/BowlingGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class BowlingGame
+	{
+		private int score = 0;
+
+		public int Score
+		{
+			get { return score; }
+		}
+
+		public void Roll(int value)
+		{
+			score += value;
+		}
+	}
+}
