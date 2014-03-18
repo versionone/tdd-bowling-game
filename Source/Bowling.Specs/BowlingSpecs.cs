1b083820d5a72a98faf2cefc438beeba54052c79
Sean McCrohan <s.m.mccrohan@gmail.com>
2014-03-18 16:15:51 -0400

    All tests pass.

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,4 +1,7 @@
+using System;
 using Bowling.Specs.Infrastructure;
+using Bowling;
+using NUnit.Framework;
 
 namespace specs_for_bowling
 {
@@ -17,4 +20,246 @@ namespace specs_for_bowling
 			_itWorked.ShouldBeTrue();
 		}
 	}
+
+    public class when_rolling_all_gutter_balls : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+            for (var i = 0; i < 20; i++)
+            {
+                card.roll(0); 
+            }           
+        }
+
+        [Specification]
+        public void score_is_0()
+        {
+            ShouldExtensionMethods.ShouldEqual(card.GetScore(), 0);
+        }
+    }
+
+    public class when_rolling_all_2s : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+            for (var i = 0; i < 20; i++)
+            {
+                card.roll(2);
+            }
+        }
+
+        [Specification]
+        public void score_is_20()
+        {
+            ShouldExtensionMethods.ShouldEqual(card.GetScore(), 40);
+        }
+    }
+
+    public class when_rolling_2s_and_5s : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+            for (var i = 0; i < 20; i++)
+            {
+                if (i % 2 == 0) { card.roll(2); }
+                else { card.roll(5); }
+            }
+        }
+
+        [Specification]
+        public void score_is_70()
+        {
+            card.GetScore().ShouldEqual(70);
+        }
+    }
+
+    public class when_rolling_spare_and_2s : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+            card.roll(8);
+            for (var i = 1; i < 20; i++)
+            {
+                card.roll(2);
+                
+            }
+        }
+
+        [Specification]
+        public void score_is_48()
+        {
+            card.GetScore().ShouldEqual(48);
+        }
+    }
+
+
+    public class when_rolling_2s_and_3s : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+            card.roll(2);
+            card.roll(2);
+            for (var i = 2; i < 20; i++)
+            {
+                card.roll(3);
+
+            }
+        }
+
+        [Specification]
+        public void score_is_58()
+        {
+            card.GetScore().ShouldEqual(58);
+        }
+    }
+
+    public class when_rolling_2_spares_and_2s : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+            card.roll(2);
+            card.roll(8);
+            card.roll(2);
+            card.roll(8);
+            for (var i = 4; i < 20; i++)
+            {
+                card.roll(2);
+            }
+        }
+
+        [Specification]
+        public void score_is_56()
+        {
+            card.GetScore().ShouldEqual(56);
+        }
+    }
+
+    public class when_rolling_too_many_frames : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+        }
+
+        [Specification, ExpectedException(typeof(TooManyRollsException))]
+        public void exception_is_thrown()
+        {
+            for (var i = 0; i < 21; i++)
+            {
+                card.roll(2);
+            }
+        }
+    }
+
+    public class when_rolling_one_strike_and_2s : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+
+            card.roll(10);
+            for (var i = 1; i < 20; i++)
+            {
+                card.roll(2);
+            }
+
+        }
+
+        [Specification]
+        public void score_is_50()
+        {
+            card.GetScore().ShouldEqual(50);
+        }
+    }
+
+    public class when_rolling_two_strike_and_2s : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+
+            card.roll(10);
+            card.roll(10);
+            for (var i = 2; i < 20; i++)
+            {
+                card.roll(2);
+            }
+
+        }
+
+        [Specification]
+        public void score_is_68()
+        {
+            card.GetScore().ShouldEqual(68);
+        }
+    }
+
+    public class when_rolling_a_perfect_game : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+            for (var i = 0; i < 12; i++)
+            {
+                card.roll(10);
+            }
+
+        }
+
+        [Specification]
+        public void score_is_300()
+        {
+            card.GetScore().ShouldEqual(300);
+        }
+    }
+
+    public class when_rolling_dutch : concerns
+    {
+        private Scorecard card;
+
+        protected override void context()
+        {
+            card = new Scorecard();
+            for (var i = 0; i < 5; i++)
+            {
+                card.roll(10);
+                card.roll(5);
+                card.roll(5);
+            }
+            card.roll(10);
+
+        }
+
+        [Specification]
+        public void score_is_200()
+        {
+            card.GetScore().ShouldEqual(200);
+        }
+    }
 }
---
+++ Source/Bowling/Scorecard.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Bowling
+{
+    public class Scorecard
+    {
+        private int[] scores;
+        private int numRolls;
+
+        public Scorecard()
+        {
+            scores = new int[24];
+            numRolls = 0;
+        }
+
+        private bool isStrike(int index)
+        {
+            return (scores[index] == 10) && (index % 2 == 0);
+        }
+
+        private bool isSpare(int index)
+        {
+            return ((scores[index] + scores[index + 1] == 10) && (scores[index]) != 10 && index % 2 == 0);
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            for(var i = 0; i < 20; i = i + 2)
+            {
+                if (isStrike(i))
+                {
+                    score = score + scores[i];
+                    score = score + scores[i + 2];
+                    if (isStrike(i + 2))
+                    {
+                        score = score + scores[i + 4];
+                    }
+                    else
+                    {
+                        score = score + scores[i + 3];
+                    }
+                }
+                else if (isSpare(i))
+                {
+                    score = score + scores[i] + scores[i + 1] + scores[i + 2];
+                }
+                else
+                {
+                    score = score + scores[i] + scores[i + 1];
+                }
+                
+            }
+
+            return score;
+        }
+
+        public void roll(int score)
+        {
+
+            if ((numRolls >= 21 && !isStrike(18)) 
+                || (numRolls >= 22 && !isStrike(20)) 
+                || (numRolls >= 24) 
+                || (numRolls >= 20 && !isStrike(16) && !isStrike(18) && !isSpare(18)) )
+            {
+                throw new TooManyRollsException();
+            }
+
+            scores[numRolls] = score;
+            if (score == 10 && numRolls%2 != 1)
+            {
+                numRolls = numRolls + 2;
+            }
+            else
+            {
+                numRolls++;
+            }
+
+
+        }
+    }
+
+    public class TooManyRollsException : Exception
+    {
+        
+    }
+}
