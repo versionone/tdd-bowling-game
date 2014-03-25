e0ef1dea5e8de5d78accf07b65728214d714a3c6
Matt Higgins <mhhiggins@gmail.com>
2014-03-25 14:05:21 -0400

    scoring of gutter balls and all 2s

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,3 +1,4 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
 
 namespace specs_for_bowling
@@ -15,6 +16,40 @@ namespace specs_for_bowling
 		public void it_works()
 		{
 			_itWorked.should_be_true("we're ready to roll!");
+		}
+	}
+
+	public class when_rolling_all_gutter_balls : concerns
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = new BowlingGame();
+			20.times(() => _game.Roll(0));
+		}
+
+		[Specification]
+		public void the_score_is_zero()
+		{
+			_game.Score().should_equal(0);
+		}
+	}
+
+	public class when_rolling_all_twos : concerns
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = new BowlingGame();
+			20.times(() => _game.Roll(2));
+		}
+
+		[Specification]
+		public void the_score_is_forty()
+		{
+			_game.Score().should_equal(40);
 		}
 	}
 }
