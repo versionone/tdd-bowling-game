7496f1e265be2171cf44a4d29fca9e524a4bf5b2
Erin Stanfill <erin.stanfill@versionone.com>
2014-03-25 14:45:12 -0400

    can handle spare

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -35,6 +35,7 @@ namespace specs_for_bowling
 			_bowlingGame.Score.ShouldEqual(0);
 		}
 	}
+
 	public class when_it_alternates_nines_and_gutters : concerns
 	{
 		private BowlingGame _bowlingGame;
@@ -45,7 +46,7 @@ namespace specs_for_bowling
 		}
 
 		[Specification]
-		public void the_score_is_ninety()
+		public void the_score_is_90()
 		{
 			10.times(() =>
 			{
@@ -53,6 +54,33 @@ namespace specs_for_bowling
 				_bowlingGame.Roll(9);
 			});
 			_bowlingGame.Score.ShouldEqual(90);
+		}
+	}
+
+	public class when_the_first_frame_is_a_spare_and_the_rest_score_2 : concerns
+	{
+		private BowlingGame _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new BowlingGame();
+			_bowlingGame.Roll(8);
+			_bowlingGame.Roll(2);
+			18.times(() => _bowlingGame.Roll(2));
+		}
+
+		[Specification]
+		public void the_first_frame_is_a_spare()
+		{
+			_bowlingGame.IsSpare(8, 2);
+			_bowlingGame.CalculateScore();
+			_bowlingGame.Frames[0].ShouldEqual(12);
+		}
+		[Specification]
+		public void the_score_is_48()
+		{
+
+			_bowlingGame.Score.ShouldEqual(48);
 		}
 	}
 }
--- Source/Bowling/BowlingGame.cs
+++ Source/Bowling/BowlingGame.cs
@@ -7,11 +7,41 @@ namespace Bowling
 {
 	public class BowlingGame
 	{
+		private List<int> rolls = new List<int>();
+		private List<int> frames = new List<int>();
+
 		public void Roll(int pinsKnockedDown)
 		{
-			Score += pinsKnockedDown;
+			rolls.Add(pinsKnockedDown);
 		}
 
-		public int Score { get; private set; }
+		public List<int> Frames
+		{
+			get { return frames; }
+		}
+
+		public int CalculateScore()
+		{
+			for (int i = 0; i < rolls.Count -1; i = i + 2)
+			{
+				
+				if (IsSpare(rolls[i], rolls[i + 1]))
+				{
+					frames.Add(rolls[i] + rolls[i + 1] + rolls[1 + 2]);
+				}
+				else
+				{
+					frames.Add(rolls[i] + rolls[i + 1]);	
+				}
+			}
+			return frames.Sum();
+		}
+
+		public bool IsSpare(int first, int second)
+		{
+			return (first + second == 10);
+		}
+
+		public int Score { get { return CalculateScore(); } }
 	}
 }
