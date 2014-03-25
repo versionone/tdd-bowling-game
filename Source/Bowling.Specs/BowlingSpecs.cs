444e7e10fc168326084bc8f9ccb4605b74a45cf1
Erin Stanfill <erin.stanfill@versionone.com>
2014-03-25 13:45:40 -0400

    Gutter Test Pass

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,3 +1,4 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
 
 namespace specs_for_bowling
@@ -15,6 +16,23 @@ namespace specs_for_bowling
 		public void it_works()
 		{
 			_itWorked.ShouldBeTrue();
+		}
+	}
+
+	public class when_its_all_gutter_balls : concerns
+	{
+		private BowlingGame _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new BowlingGame();
+		}
+
+		[Specification]
+		public void the_score_is_zero()
+		{
+			20.times(() => _bowlingGame.Roll(0));
+			_bowlingGame.Score.ShouldEqual(0);
 		}
 	}
 }
---
+++ Source/Bowling/BowlingGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class BowlingGame
+	{
+		public void Roll(int i)
+		{
+			
+		}
+
+		public int Score { get; private set; }
+	}
+}
