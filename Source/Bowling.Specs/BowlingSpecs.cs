dee33370a1d0621f3834ee3506a1cd9da8cea827
Matt Higgins <mhhiggins@gmail.com>
2014-03-28 13:56:33 -0400

    scoring of two spares followed by 2s

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -113,4 +113,27 @@ namespace specs_for_bowling
 			_game.Score().ShouldEqual(48);
 		}
 	}
+
+	public class when_rolling_two_spares_followed_by_2s : concerns
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = new BowlingGame();
+
+			_game.Roll(2);
+			_game.Roll(8);
+			_game.Roll(2);
+			_game.Roll(8);
+
+			16.times(() => _game.Roll(2));
+		}
+
+		[Specification]
+		public void the_score_should_be()
+		{
+			_game.Score().ShouldEqual(56);
+		}
+	}
 }
