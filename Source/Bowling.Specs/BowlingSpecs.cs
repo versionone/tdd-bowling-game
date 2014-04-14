e6d9b8d13f4cd84f670a549449ad0929458d2f03
Erin Stanfill <erin.stanfill@versionone.com>
2014-04-14 16:03:13 -0400

    Amitai's solution

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,20 +1,135 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
+using NUnit.Framework;
+using System;
 
 namespace specs_for_bowling
 {
-	public class when_everything_is_wired_up : concerns
+	public class when_its_all_gutters : concerns
 	{
-		private bool _itWorked;
+		Game game = new Game();
 
 		protected override void context()
 		{
-			_itWorked = true;
+			20.times(() => game.Roll(0));
 		}
 
 		[Specification]
-		public void it_works()
+		public void then_the_score_is_0()
 		{
-			_itWorked.ShouldBeTrue();
+			game.Score.ShouldEqual(0);
+		}
+	}
+
+	public class when_its_all_2s : concerns
+	{
+		Game game = new Game();
+
+		protected override void context()
+		{
+			20.times(() => game.Roll(2));
+		}
+		[Specification]
+		public void then_the_score_is_40()
+		{
+			game.Score.ShouldEqual(40);
+		}
+	}
+
+	public class when_the_first_frame_is_a_spare_and_the_remaining_rolls_are_all_2: concerns
+	{
+		private Game game = new Game();
+
+		protected override void context()
+		{
+			game.Roll(8);
+			19.times(() => game.Roll(2));
+		}
+
+		[Specification]
+		public void then_the_score_is_48()
+		{
+			game.Score.ShouldEqual(48);
+		}
+	}
+
+	public class when_the_first_two_frames_are_spare_and_the_remaining_rolls_are_all_2 : concerns
+	{
+		private Game game = new Game();
+
+		protected override void context()
+		{
+			game.Roll(2);
+			game.Roll(8);
+			game.Roll(2);
+			game.Roll(8);
+			16.times(() => game.Roll(2));
+		}
+
+		[Specification]
+		public void then_the_score_is_56()
+		{
+			game.Score.ShouldEqual(56);
+		}
+	}
+
+	public class when_10_frames_are_bowled : concerns
+	{
+		private Game game = new Game();
+
+		[Specification]
+		[ExpectedException(typeof(Exception))]
+		public void then_20_rolls_with_no_bonuses()
+		{
+			20.times(() =>game.Roll(4));
+			game.Roll(4);
+		}
+
+		[Specification]
+		[ExpectedException(typeof(Exception))]
+		public void then_12_rolls_with_all_strikes()
+		{
+			12.times(() => game.Roll(10));
+			game.Roll(4);
+		}
+
+		[Specification]
+		[ExpectedException(typeof(Exception))]
+		public void then_21_rolls_with_all_spares()
+		{
+			21.times(() => game.Roll(5));
+			game.Roll(4);
+		}
+		[Specification]
+		public void count_completed_frames()
+		{
+			game.Roll(2);
+			game.CountCompletedFrames().ShouldEqual(0);
+			game.Roll(8);
+			game.CountCompletedFrames().ShouldEqual(0);
+			game.Roll(2);
+			game.CountCompletedFrames().ShouldEqual(1);
+			game.Roll(8);
+			game.CountCompletedFrames().ShouldEqual(1);
+			16.times(() => game.Roll(2));
+			game.CountCompletedFrames().ShouldEqual(10);
+		}
+	}
+
+	public class when_the_first_frame_is_a_strike : concerns
+	{
+		private Game game = new Game();
+
+		protected override void context()
+		{
+			game.Roll(10);
+			18.times(() => game.Roll(2));
+		}
+
+		[Specification]
+		public void then_the_score_is_50()
+		{
+			game.Score.ShouldEqual(50);
 		}
 	}
 }
---
+++ Source/Bowling/Game.cs
@@ -0,0 +1,103 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bowling
+{
+	public class Game
+	{
+		private int _score = 0;
+		List<int> Rolls = new List<int>();
+
+		public Game()
+		{
+		}
+
+		public void Roll(int pins)
+		{
+			if (CountCompletedFrames() == 10)
+				throw new Exception();
+
+			Rolls.Add(pins);
+
+		}
+
+		private bool firstRollsAreBonus(List<int> rolls)
+		{
+			return rolls.Take(1).Sum() == 10 || rolls.Take(2).Sum() == 10;
+		}
+
+		private bool onLastFrame()
+		{
+			return (Rolls.Count == (20 - CountStrikes()));
+		}
+		public int CountStrikes()
+		{
+			return Rolls.Count(x => x == 10);
+		}
+
+		public int CountCompletedFrames()
+		{
+			return CountCompletedFrames(Rolls);
+		}
+
+		public int CountCompletedFrames(List<int> rolls)
+		{
+			if (rolls.Count < 2)
+				return 0;
+
+			//Strike
+			if (rolls[0] == 10 && rolls.Count >= 3)
+			{
+				return 1 + CountCompletedFrames(rolls.Skip(1).ToList());
+			}
+
+			//Spare
+			if (firstRollsAreBonus(rolls) && rolls.Count >= 3)
+			{
+				return 1 + CountCompletedFrames(rolls.Skip(2).ToList());
+			}
+
+			if (!firstRollsAreBonus(rolls))
+				return 1 + CountCompletedFrames(rolls.Skip(2).ToList());
+
+			return 0;
+		}
+
+		public int CalculateScore()
+		{
+			if (CountCompletedFrames() < 10)
+				return 0;
+			return CalculateScore(Rolls);
+		}
+
+		public int CalculateScore(List<int> rolls)
+		{
+			if (rolls.Count == 0)
+				return 0;
+
+			//Strike
+			if (rolls[0] == 10 )
+			{
+				return rolls.Take(3).Sum() + CalculateScore(rolls.Skip(1).ToList());
+			}
+
+			//Spare
+			if (firstRollsAreBonus(rolls))
+			{
+				return rolls.Take(3).Sum() + CalculateScore(rolls.Skip(2).ToList());
+			}
+
+			if (!firstRollsAreBonus(rolls))
+				return rolls.Take(2).Sum() + CalculateScore(rolls.Skip(2).ToList());
+
+			return 0;
+		}
+		public int Score
+		{
+
+			get { return CalculateScore(); }
+		}
+	}
+}
