7f4cab1f8d118e52152c1d3ccad6b296e5e8ce07
Pavel Mamut <pavel@versionone.com>
2014-04-24 14:42:47 -0400

    all 2s

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -19,4 +19,21 @@ namespace specs_for_bowling
 			_game.Score.ShouldEqual(0);
 		}
 	}
+
+	public class when_rolling_all_twos : concerns<BowlingGame>
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = build_up();
+			20.times(index => _game.Roll(2));
+		}
+
+		[Specification]
+		public void the_score_should_be_fourty()
+		{
+			_game.Score.ShouldEqual(40);
+		}
+	}
 }
--- Source/Bowling/BowlingGame.cs
+++ Source/Bowling/BowlingGame.cs
@@ -11,7 +11,7 @@ namespace Bowling
 
 		public void Roll(int pins)
 		{
-			
+			Score += pins;
 		}
 	}
 }
