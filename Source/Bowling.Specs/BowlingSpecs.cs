ac3ee62a989dc5d22108612f81d92d7b1590b2bb
Pavel Mamut <pavel@versionone.com>
2014-05-08 13:44:43 -0400

    two twos followed by all threes

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -52,4 +52,23 @@ namespace specs_for_bowling
 			_game.Score.ShouldEqual(40);
 		}
 	}
+
+
+	public class when_rolling_two_twos_followed_by_all_threes : concerns<BowlingGame>
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = build_up();
+			2.times(() => _game.Roll(2));
+			18.times(() => _game.Roll(3));
+		}
+
+		[Specification]
+		public void score_should_be_fifty_eight()
+		{
+			_game.Score.ShouldEqual(58);
+		}
+	}
 }
