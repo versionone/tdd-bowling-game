f6179619f0ba5fc03e8755b294e9ef196af441d2
Pavel Mamut <pavel@versionone.com>
2014-05-19 10:38:46 -0400

    Will: "Updated my solution to fix the case that was failing.  I also refactored it a little to be more clear."

--- Source/Bowling/BowlingGame.cs
+++ Source/Bowling/BowlingGame.cs
@@ -8,56 +8,115 @@ using System.Text;
 
 namespace Bowling
 {
-	public class BowlingGame
-	{
-		public int Score
-		{
-			get
-			{
-				int score = 0;
-				int frame = 1;
-				int ball = 1;
-				for(int i = 0; i < _rolls.Count; i++)
-				{
-					score += _rolls[i];
+    public class BowlingGame
+    {
+        public int Score { get; set; }
+        private int Frame { get; set; }
+        private int Ball { get; set; }
+        private int LastRoll { get; set; }
+        private int SecondToLastRoll { get; set; }
 
-					if(ball == 1 && _rolls[i] == 10 && frame < 10) // Strike
-					{
-						score += _rolls[i + 1] + _rolls[i + 2];
-					}
-					else if (ball == 2 && (_rolls[i - 1] + _rolls[i]) == 10) // Spare
-					{
-						score += _rolls[i + 1];
-					}
+        public BowlingGame()
+        {
+            Score = 0;
+            Frame = 1;
+            Ball = 1;
+            LastRoll = 0;
+            SecondToLastRoll = 0;
+        }
 
-					ball++;
-					if(ball == 2 && _rolls[i] == 10 || frame < 10 && ball > 2)
-					{
-						ball = 1;
-						frame++;
-					}
-				}
-				return score;
-			}
-		}
+        public void Roll(int pins)
+        {
+            Debug.Print("{0} {1} - {2}", Frame, Ball, pins);
+            CheckGameOver();
+            UpdateScore(pins);
+            UpdateLastRolls(pins);
+            UpdateBallAndFrame();
+            Debug.Print(" Score: {0}", Score);
+        }
 
-		private List<int> _rolls = new List<int>();
+        private void UpdateScore(int pins)
+        {
+            /*
+             * STLR LR  MULT
+             * X    X   3
+             * n    /   2
+             * n    X   2
+             * X    n   2
+             * else     1
+             */
+            int multiplier = 1;
+            if (!TenthFrame || Ball < 3)
+            {
+                if (LastRoll == 10 && SecondToLastRoll == 10)
+                {
+                    multiplier = 3;
+                }
+                else if (Ball == 1 && (LastRoll + SecondToLastRoll) == 10 || LastRoll == 10 || SecondToLastRoll == 10)
+                {
+                    multiplier = 2;
+                }
 
-		public void Roll(int pins)
-		{
-			CheckGameOver();
-			UpdateRollsArray(pins);
-		}
+                // Handle second ball in tenth frame special case
+                if (TenthFrame && Ball == 2 && LastRoll == 10)
+                {
+                    multiplier--;
+                }
+            }
 
-		private void UpdateRollsArray(int pins)
-		{
-			_rolls.Add(pins);
-		}
+            Score += multiplier * pins;
+        }
 
-		private void CheckGameOver()
-		{
-			if(_rolls.Count > 19)
-				throw new InvalidOperationException("Game Over");
-		}
-	}
+        private void UpdateLastRolls(int pins)
+        {
+            SecondToLastRoll = LastRoll;
+            LastRoll = pins;
+        }
+
+        private void UpdateBallAndFrame()
+        {
+            Ball++;
+            if (FrameComplete)
+            {
+                Ball = 1;
+                Frame++;
+            }
+        }
+
+        private bool FrameComplete
+        {
+            get
+            {
+                /*
+                 * FrameComplete if:
+                 * - non-tenth frame:
+                 *   - We roll two balls
+                 *   - The first ball we roll is a strike
+                 * - tenth frame:
+                 *  - We roll three balls
+                 *  - We roll two balls with no marks
+                 */
+                if (TenthFrame)
+                {
+                    return Ball > 3 || Ball == 3 && !(LastRoll == 10 || SecondToLastRoll == 10 || (SecondToLastRoll + LastRoll) == 10);
+                }
+                else
+                {
+                    return Ball > 2 || LastRoll == 10;
+                }
+            }
+        }
+
+        private bool TenthFrame
+        {
+            get { return Frame == 10; }
+        }
+
+        private void CheckGameOver()
+        {
+            // If we are done with the tenth frame already, throw an exception
+            if (Frame > 10)
+                throw new InvalidOperationException("Game Over");
+        }
+    }
 }
