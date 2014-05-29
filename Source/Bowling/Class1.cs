1ce151f61de0faf60e603cf1eec4a631e4de5cf6
rajiv <rajiv.delwadia@versionone.com>
2014-05-29 11:14:53 -0400

    tried to restore Class1.cs from memory

---
+++ Source/Bowling/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Game
+{
+	public int score;
+	public int rollNum;
+	public int strike;
+	public int lastRoll;
+	public int lastFrame;
+
+	public Game()
+	{
+		rollNum = 1;
+	}
+
+	public void Roll(int n) 
+	{
+		/*if (rollNum >= 21)
+			throw new Exception();*/
+
+		if (IsLastRollOfFrame())
+		{
+			lastFrame = lastRoll + n;
+		}
+		else 
+		{
+			if (LastFrameWasSpare())
+			{
+				score = score + n;
+			}
+			else if (strike > 0)
+			{
+				for (int i = 1; i <= strike; i++)
+					score = score + n;
+			}
+			if (10 == n)
+			{
+				strike++;
+				rollNum++;
+			}
+			else
+			{
+				strike--;
+			}
+		}
+
+		score = score + n;
+		rollNum++;
+		lastRoll = n;
+	}
+
+	private bool IsLastRollOfFrame()
+	{
+		return 0 == (rollNum%2);
+	}
+
+	private bool LastFrameWasSpare()
+	{
+		return 10 == lastFrame;
+	}
+}
