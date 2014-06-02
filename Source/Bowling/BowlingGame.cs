c6e04db84cc63df1b8035fc30091837b781dfaba
Pavel Mamut <pavel@versionone.com>
2014-06-02 15:17:27 -0400

    refactoring for clarity

--- Source/Bowling/BowlingGame.cs
+++ Source/Bowling/BowlingGame.cs
@@ -7,27 +7,43 @@ namespace Bowling
 	{
 		public void Roll(int pins)
 		{
-			if(_frameCount == 10)
-				throw new InvalidOperationException(("game over"));
+			CheckGameOver();
 
-			_rollCount++;
-			var firstBall = _rollCount % 2 != 0;
+			var firstBall = _rollCount % 2 == 0;
 
 			if(firstBall)
 			{
-				if(_lastFrameScore == 10)
-				{
-					Score += pins;
-				}
-				_lastFrameScore = 0;
+				HandleLastFrameBonus(pins);
 			}
 			else
 			{
 				_frameCount++;
 			}
 
+			_rollCount++;
 			Score += pins;
 			_lastFrameScore += pins;
+		}
+
+		private void HandleLastFrameBonus(int pins)
+		{
+			if (LastFrameWasASpare)
+			{
+				Score += pins;
+			}
+
+			_lastFrameScore = 0;
+		}
+
+		private void CheckGameOver()
+		{
+			if (_frameCount == 10)
+				throw new InvalidOperationException(("game over"));
+		}
+
+		private bool LastFrameWasASpare
+		{
+			get { return _lastFrameScore == 10; }
 		}
 
 		private int _lastFrameScore;
@@ -36,8 +52,5 @@ namespace Bowling
 
 		private int _rollCount;
 		private int _frameCount;
-
-
-
 	}
 }
