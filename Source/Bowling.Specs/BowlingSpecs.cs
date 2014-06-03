eaba9f3d35840e6e4ad402f70b23bc6fc27bf3b1
jamesashley1 <jamesashley@imaginativeuniversal.com>
2014-06-02 22:56:53 -0400

    tests pass through two frames are strikes

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,20 +1,182 @@
+using System;
+using Bowling;
 using Bowling.Specs.Infrastructure;
+using NUnit.Framework;
 
 namespace specs_for_bowling
 {
-	public class when_everything_is_wired_up : concerns
-	{
-		private bool _itWorked;
+    public class when_everything_is_wired_up : concerns
+    {
+        private bool _itWorked;
 
-		protected override void context()
-		{
-			_itWorked = true;
-		}
+        protected override void context()
+        {
+            _itWorked = true;
+        }
 
-		[Specification]
-		public void it_works()
-		{
-			_itWorked.ShouldBeTrue();
-		}
-	}
+        [Specification]
+        public void it_works()
+        {
+            _itWorked.ShouldBeTrue();
+        }
+    }
+
+    public class when_rolling_all_gutter_balls : concerns<BowlingGame>
+    {
+        private BowlingGame _game;
+
+        protected override void context()
+        {
+            _game = build_up();
+            20.times(() => _game.Roll(0));
+        }
+
+        [Specification]
+        public void the_score_is_zero()
+        {
+            _game.Score.ShouldEqual(0);
+        }
+    }
+    public class when_rolling_all_twos : concerns<BowlingGame>
+    {
+        private BowlingGame _game;
+
+        protected override void context()
+        {
+            _game = build_up();
+            20.times(() => _game.Roll(2));
+        }
+
+        [Specification]
+        public void the_score_is_forty()
+        {
+            _game.Score.ShouldEqual(40);
+        }
+    }
+
+    public class when_rolling_first_two_are_two_rest_are_three : concerns<BowlingGame>
+    {
+        private BowlingGame _game;
+
+        protected override void context()
+        {
+            _game = build_up();
+            2.times(() => _game.Roll(2));
+            18.times(() => _game.Roll(3));
+        }
+
+        [Specification]
+        public void the_score_is_fifty_eight()
+        {
+            _game.Score.ShouldEqual(58);
+        }
+    }
+
+    public class when_rolling_alternating_twos_and_fives : concerns<BowlingGame>
+    {
+        private BowlingGame _game;
+
+        protected override void context()
+        {
+            _game = build_up();
+            10.times(() => { _game.Roll(2); _game.Roll(5); });
+        }
+
+        [Specification]
+        public void the_score_is_seventy()
+        {
+            _game.Score.ShouldEqual(70);
+        }
+    }
+
+    public class when_rolling_first_frame_is_spare_all_other_rolls_are_two : concerns<BowlingGame>
+    {
+        private BowlingGame _game;
+
+        protected override void context()
+        {
+            _game = build_up();
+            2.times(() => { _game.Roll(5); });
+            18.times(() => { _game.Roll(2); });
+
+        }
+
+        [Specification]
+        public void the_score_is_forty_eight()
+        {
+            _game.Score.ShouldEqual(48);
+        }
+    }
+
+    public class when_rolling_first_two_frames_are_two_eight_split_all_other_rolls_are_two : concerns<BowlingGame>
+    {
+        private BowlingGame _game;
+
+        protected override void context()
+        {
+            _game = build_up();
+            2.times(() => { _game.Roll(2); _game.Roll(8); });
+            16.times(() => { _game.Roll(2); });
+        }
+
+        [Specification]
+        public void the_score_is_fifty_six()
+        {
+            _game.Score.ShouldEqual(56);
+        }
+    }
+
+    public class when_ten_frames_are_bowled : concerns<BowlingGame>
+    {
+        private BowlingGame _game;
+
+        protected override void context()
+        {
+            _game = build_up();
+            20.times(() => { _game.Roll(2); });
+        }
+
+        [Specification]
+        [ExpectedException(typeof(InvalidOperationException), ExpectedMessage = "game over")]
+        public void prevent_further_frames()
+        {
+            _game.Roll(2);
+        }
+
+        public class when_rolling_first_frame_is_a_strike_other_rolls_are_two : concerns<BowlingGame>
+        {
+            private BowlingGame _game;
+
+            protected override void context()
+            {
+                _game = build_up();
+                _game.Roll(10);
+                18.times(() => _game.Roll(2));
+            }
+
+            [Specification]
+            public void the_score_is_fifty()
+            {
+                _game.Score.ShouldEqual(50);
+            }
+        }
+
+        public class when_rolling_first_two_frames_are_strikes_other_rolls_are_two : concerns<BowlingGame>
+        {
+            private BowlingGame _game;
+
+            protected override void context()
+            {
+                _game = build_up();
+                2.times(() => _game.Roll(10));
+                16.times(() => _game.Roll(2));
+            }
+
+            [Specification]
+            public void the_score_is_sixty_eight()
+            {
+                _game.Score.ShouldEqual(68);
+            }
+        }
+    }
 }
---
+++ Source/Bowling/BowlingGame.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net.Configuration;
+
+namespace Bowling
+{
+    public class BowlingGame
+    {
+        public void Roll(int pins)
+        {
+            CheckGameOver();
+
+            var isNewFrame = _rollCount % 2 == 0;
+
+            if (isNewFrame)
+            {
+                HandleSpareBonus(pins);
+                CheckStrike(pins);
+            }
+            else
+            {
+                HandleStrikeBonuses(pins);
+            }
+
+            HandleCounters(pins, isNewFrame);
+
+        }
+
+        private void HandleSpareBonus(int pins)
+        {
+            if (LastFrameWasASpare)
+            {
+                Score += pins;
+            }
+        }
+
+        private void HandleStrikeBonuses(int pins)
+        {
+            if (LastFrameWasAStrike)
+            {
+                if(_consecutiveStrikes)
+                {
+                    Score += 10 + _lastRollScore;
+                }
+                Score += _lastRollScore + pins;
+                LastFrameWasAStrike = false;
+            }
+        }
+
+        private void CheckStrike(int pins)
+        {
+            if (pins == 10)
+            {
+                if(LastFrameWasAStrike)
+                {
+                    _consecutiveStrikes = true;
+                }
+                
+                _rollCount++;
+                LastFrameWasAStrike = true;
+            }
+        }
+
+        private void HandleCounters(int pins, bool isNewFrame)
+        {
+            _rollCount++;
+            Score += pins;
+
+            if (isNewFrame)
+                _lastFrameScore = 0;
+
+            _lastRollScore = pins;
+            _lastFrameScore += _lastRollScore;
+
+        }
+
+        private void CheckGameOver()
+        {
+            if (FrameCount >= 10)
+                throw new InvalidOperationException(("game over"));
+        }
+
+        private bool LastFrameWasASpare
+        {
+            get { return _lastFrameScore == 10 && _lastRollScore < 10; }
+        }
+
+        private bool LastFrameWasAStrike
+        { get; set; }
+
+        private int _lastFrameScore;
+        private int _lastRollScore;
+
+        public int Score { get; private set; }
+
+        private int _rollCount;
+        private int FrameCount { get { return _rollCount / 2; } }
+
+        private bool _consecutiveStrikes;
+    }
+}
