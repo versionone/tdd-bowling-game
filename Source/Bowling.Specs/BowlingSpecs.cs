5b0ffd7172e185ad6938091ded7f1352c22ef289
Pavel Mamut <pavel@versionone.com>
2014-06-16 15:32:51 -0400

    freebies!

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -36,4 +36,41 @@ namespace specs_for_bowling
 			_game.GetScore().ShouldEqual(40);
 		}
 	}
+	public class when_rolling_two_2s_and_the_rest_3s : concerns<BowlingGame>
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = build_up();
+			2.times(() => _game.Roll(2));
+			18.times(() => _game.Roll(3));
+		}
+
+		[Specification]
+		public void the_score_is_58()
+		{
+			_game.GetScore().ShouldEqual(58);
+		}
+	}
+	public class when_rolling_alternating_2s_and_5s : concerns<BowlingGame>
+	{
+		private BowlingGame _game;
+
+		protected override void context()
+		{
+			_game = build_up();
+			10.times(() =>
+			{
+				_game.Roll(2);
+				_game.Roll(5);
+			});
+		}
+
+		[Specification]
+		public void the_score_is_70()
+		{
+			_game.GetScore().ShouldEqual(70);
+		}
+	}
 }
