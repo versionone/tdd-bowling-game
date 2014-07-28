d2306cfd24c45894b393eed6cc89b7425f4731b9
Matt Higgins <mhhiggins@gmail.com>
2014-07-28 14:36:48 -0400

    gutter balls

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,4 +1,5 @@
 using Bowling.Specs.Infrastructure;
+using Bowling;
 
 namespace specs_for_bowling
 {
@@ -17,4 +18,22 @@ namespace specs_for_bowling
 			_itWorked.ShouldBeTrue();
 		}
 	}
+
+    public class when_rolling_all_gutter_balls : concerns
+    {
+        private BowlingGame _game;
+
+        protected override void context()
+        {
+            _game = new BowlingGame();
+
+            20.times(() => _game.Bowl(0));
+        }
+
+        [Specification]
+        public void the_score_is_0()
+        {
+            _game.Score.ShouldEqual(0);
+        }
+    }
 }
---
+++ Source/Bowling/BowlingGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+    public class BowlingGame
+    {
+        public void Bowl(int pins)
+        {
+        }
+
+        public int Score { get { return 0; } }
+    }
+}
