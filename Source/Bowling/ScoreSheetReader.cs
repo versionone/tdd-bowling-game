e62544a35098d16a100625a5f9d6306ccdd9a9ba
unknown <kgargano@kgargano1.corp.versionone.net>
2014-11-21 11:54:55 -0500

    Implemented handling spares.

--- Source/Bowling/ScoreSheetReader.cs
+++ Source/Bowling/ScoreSheetReader.cs
@@ -10,6 +10,28 @@ namespace Bowling
 	{			
 		public int FirstRoll { get; set; }
 		public int SecondRoll { get; set; }
+
+		public enum FrameType
+		{
+			Spare,
+			Strike,
+			Normal
+		}
+
+		public FrameType GetFrameType()
+		{
+			var type = FrameType.Normal;
+			if (this.FirstRoll == 10)
+			{
+				type = FrameType.Strike;
+			}
+			else if (this.FirstRoll + this.SecondRoll == 10)
+			{
+				type = FrameType.Spare;
+			}
+			return type;
+		}
+
 	}
 
 	public class ScoreSheet
@@ -29,10 +51,27 @@ namespace Bowling
 		public int CalculateScore()
 		{
 			int result = 0;
-			foreach (var frame in Frames)
+			/*foreach (var frame in Frames)
 			{
 				result += CalculateScore(frame);
+			}*/
+
+			for (var i = 0; i < Frames.Count; i++)
+			{
+				var type = Frames[i].GetFrameType();
+				switch (type)
+				{
+					case Frame.FrameType.Normal:
+						result += CalculateScore(Frames[i]);
+						break;
+					case Frame.FrameType.Spare:
+						//TODO what happens if spare is in last frame?
+						result += (CalculateScore(Frames[i]) + Frames[i + 1].FirstRoll);
+						break;
+				}
+
 			}
+
 			return result;
 		}
 
