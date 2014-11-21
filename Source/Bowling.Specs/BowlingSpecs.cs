1485f611b4c7f24449b912f4781df073e4b8402d
unknown <kgargano@kgargano1.corp.versionone.net>
2014-11-21 11:28:36 -0500

    in medias res -- oops, just remembered to branch!

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,20 +1,150 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
+using StructureMap.Pipeline;
 
 namespace specs_for_bowling
 {
-	public class when_everything_is_wired_up : concerns
+	/*
+	 * 
+	 * # TDD Bowling Game
+An example of doing Test-Driven Development using Bowling as the domain.
+
+## The game to be played
+Below are some scenarios we can use to drive the development of the game.
+
+* when rolling all gutter balls, the score is 0.
+* when rolling all 2s, the score is 40.
+* when the first 2 rolls are 2 and the rest are 3, the score is 58.
+* when rolling alternating 2s and 5s, the score 70.
+* when the first frame is a spare and the remaining rolls are all 2, the score is 48.
+* when the first 2 frames are spare (as 2,8) and the rest score 2, the score is 56.
+* when 10 frames have been bowled, don't allow any more to be bowled.
+* when the first frame is a strike and the rest score 2, the score is 50.
+* when the first 2 frames are strikes and the rest score 2, the score is 68.
+* when rolling a perfect game, the score is 300.
+* when rolling alternate strikes and spares, the score is 200.
+	 * 
+	 */
+
+	public class when_rolling_all_gutter_balls : concerns
 	{
-		private bool _itWorked;
+		private int? _score;
 
 		protected override void context()
 		{
-			_itWorked = true;
+			ScoreSheet scoreSheet = new ScoreSheet();
+			_score = scoreSheet.CalculateScore();
 		}
 
 		[Specification]
-		public void it_works()
+		public void the_score_should_be_zero()
 		{
-			_itWorked.ShouldBeTrue();
+			_score.ShouldEqual(0);
 		}
 	}
+
+	public class when_rolling_all_gutter_balls_for_three_frames : concerns
+	{
+		private int? _score;
+		private ScoreSheet scoreSheet;
+
+		protected override void context()
+		{
+			scoreSheet = new ScoreSheet();
+			scoreSheet.AddFrame(new Frame());
+			scoreSheet.AddFrame(new Frame());
+			scoreSheet.AddFrame(new Frame());
+
+			_score = scoreSheet.CalculateScore();
+		}
+
+		[Specification]
+		public void there_are_three_frames()
+		{
+			scoreSheet.Frames.Count.ShouldEqual(3);
+		}
+
+		[Specification]
+		public void the_score_should_be_zero()
+		{
+			_score.ShouldEqual(0);
+		}
+	}
+
+	public class when_rolling_all_twos : concerns
+	{
+		private int? _score;
+		private ScoreSheet scoreSheet;
+
+		protected override void context()
+		{
+			scoreSheet = new ScoreSheet();
+			for (var i = 0; i < 10; i++)
+			{
+				scoreSheet.AddFrame(new Frame() {FirstRoll = 2, SecondRoll = 2});
+			}
+			_score = scoreSheet.CalculateScore();
+		}
+
+		[Specification]
+		public void the_score_should_be_forty()
+		{
+			_score.ShouldEqual(40);
+		}
+	}
+
+	public class when_rolling_twos_for_the_first_two_frames_and_threes_for_the_remaining_frames : concerns
+	{
+		private int? _score;
+		private ScoreSheet scoreSheet;
+
+		protected override void context()
+		{
+			scoreSheet = new ScoreSheet();
+			scoreSheet.AddFrame(new Frame() { FirstRoll = 2, SecondRoll = 2 });
+			for (var i = 1; i < 10; i++)
+			{
+				scoreSheet.AddFrame(new Frame(){ FirstRoll = 3, SecondRoll = 3});
+			}
+			_score = scoreSheet.CalculateScore();
+		}
+
+		[Specification]
+		public void the_score_should_be_fifty_eight()
+		{
+			_score.ShouldEqual(58);
+		}
+	}
+
+	public class when_rolling_alternating_twos_and_fives : concerns
+	{
+		private int? _score;
+		private ScoreSheet scoreSheet;
+
+		protected override void context()
+		{
+			scoreSheet = new ScoreSheet();
+			for (var i = 0; i < 10; i++)
+			{
+				if (i%2 != 0)
+				{
+					scoreSheet.AddFrame(new Frame() {FirstRoll = 2, SecondRoll = 2});
+				}
+				else
+				{
+					scoreSheet.AddFrame(new Frame() {FirstRoll = 5, SecondRoll = 5});
+				}
+			}
+			_score = scoreSheet.CalculateScore();
+		}
+
+		[Specification]
+		public void the_score_should_be_seventy()
+		{
+			_score.ShouldEqual(70);
+		}
+	}
+
+
+
 }
---
+++ Source/Bowling/ScoreSheetReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Bowling
+{
+	public class Frame
+	{			
+		public int FirstRoll { get; set; }
+		public int SecondRoll { get; set; }
+	}
+
+	public class ScoreSheet
+	{
+		public List<Frame> Frames { get; set; }
+
+		public ScoreSheet()
+		{
+			Frames = new List<Frame>();
+		}
+
+		public void AddFrame(Frame frame)
+		{
+			Frames.Add(frame);
+		}
+
+		public int CalculateScore()
+		{
+			int result = 0;
+			foreach (var frame in Frames)
+			{
+				result += CalculateScore(frame);
+			}
+			return result;
+		}
+
+		public int CalculateScore(Frame frame)
+		{
+			return frame.FirstRoll + frame.SecondRoll;
+		}
+	}
+}
