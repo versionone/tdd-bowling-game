30e6e6c860911d67e3fbdfee50f543f804bc3d38
Pavel Mamut <pavel@versionone.com>
2014-11-24 15:11:39 -0500

    alternating strikes and spares green - super freebie!

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -203,6 +203,28 @@ namespace specs_for_bowling
 			_game.GetScore().ShouldEqual(300);
 		}
 	}
+
+	public class when_rolling_alternating_strikes_and_spares : concerns
+	{
+		private Game _game;
+		protected override void context()
+		{
+			_game = new Game();
+			5.times(() =>
+			{
+				_game.Roll(10);
+				_game.Roll(2);
+				_game.Roll(8);
+			});
+			_game.Roll(10);
+		}
+
+		[Specification]
+		public void the_score_is_200()
+		{
+			_game.GetScore().ShouldEqual(200);
+		}
+	}
 }
 
 namespace Bowling
