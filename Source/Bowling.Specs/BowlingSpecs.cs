f5e10c3af35f965b809481b91fc4fb3be84613ad
Pavel Mamut <pavel@versionone.com>
2015-01-27 17:37:49 -0500

    when the first 2 frames are strikes and the rest score 2

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -199,4 +199,25 @@ namespace specs_for_bowling
 			_score.ShouldEqual(50);
 		}
 	}
+
+	public class when_the_first_2_frames_are_strikes_and_the_rest_score_2 : concerns
+	{
+		private int _score;
+
+		protected override void context()
+		{
+			var _game = new BowlingGame();
+
+			2.times(() => _game.Roll(10));
+			16.times(() => _game.Roll(2));
+
+			_score = _game.Score;
+		}
+
+		[Specification]
+		public void the_score_is_68()
+		{
+			_score.ShouldEqual(68);
+		}
+	}
 }
