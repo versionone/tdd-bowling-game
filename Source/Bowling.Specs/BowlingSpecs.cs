90b9c275cc1abd4ad67864ccac5651937e908ccf
Pavel Mamut <pavel@versionone.com>
2015-02-11 15:35:15 -0500

    Modified BowlingGame to handle spares and added additional tests.

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -54,4 +54,64 @@ namespace specs_for_bowling
 			_score.ShouldEqual(40);
 		}
 	}
+
+	public class when_the_first_2_rolls_are_2_and_the_rest_are_3 : concerns
+	{
+		private int _score;
+
+		protected override void context()
+		{
+			var game = new BowlingGame();
+			2.times(() => game.Roll(2));
+			18.times(() => game.Roll(3));
+			_score = game.Score;
+		}
+
+		[Specification]
+		public void the_score_is_58()
+		{
+			_score.ShouldEqual(58);
+		}
+	}
+	public class when_rolling_alternating_2s_and_5s : concerns
+	{
+		private int _score;
+
+		protected override void context()
+		{
+			var game = new BowlingGame();
+			10.times(() =>
+			{
+				game.Roll(2);
+				game.Roll(5);
+			});
+			_score = game.Score;
+		}
+
+		[Specification]
+		public void the_score_is_70()
+		{
+			_score.ShouldEqual(70);
+		}
+	}
+
+	public class when_rolling_a_spare_following_by_all_2s : concerns
+	{
+		private int _score;
+
+		protected override void context()
+		{
+			var game = new BowlingGame();
+			game.Roll(5);
+			game.Roll(5);
+			18.times(() => game.Roll(2));
+			_score = game.Score;
+		}
+
+		[Specification]
+		public void the_score_is_48()
+		{
+			_score.ShouldEqual(48);
+		}
+	}
 }
--- Source/Bowling/BowlingGame.cs
+++ Source/Bowling/BowlingGame.cs
@@ -2,9 +2,30 @@ namespace Bowling
 {
 	public class BowlingGame
 	{
+		private int TotalRolls;
+		private int LastRoll;
+		private bool IsLastFrameSpare;
+
 		public void Roll(int pins)
 		{
 			Score += pins;
+
+			if (TotalRolls % 2 == 0)
+			{
+				if (IsLastFrameSpare)
+				{
+					Score += pins;
+				}
+
+				IsLastFrameSpare = false;
+			}
+			else if (LastRoll + pins == 10)
+			{
+				IsLastFrameSpare = true;
+			}
+
+			TotalRolls++;
+			LastRoll = pins;
 		}
 
 		public int Score { get; private set; }
