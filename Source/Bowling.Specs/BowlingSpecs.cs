16aa6ac419cfd9dff72132efab96256ae1e403a2
rajiv <rajiv.delwadia@versionone.com>
2015-02-13 11:16:26 -0500

    Got gutter balls and all two's knocked out.

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,3 +1,4 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
 
 namespace specs_for_bowling
@@ -17,4 +18,44 @@ namespace specs_for_bowling
 			_itWorked.ShouldBeTrue();
 		}
 	}
+
+	public class when_rolling_all_gutter_balls : concerns
+	{
+		private Game _game;
+		protected override void context()
+		{
+			_game = new Game();
+			for (int i = 1; i <= 20; i++)
+			{
+				_game.Roll(0);
+			}
+		}
+
+		[Specification]
+		public void the_score_is_zero()
+		{
+			_game.Score.ShouldEqual(0);
+		}
+	}
+
+	public class when_rolling_all_2s : concerns
+	{
+		private Game _game;
+
+		protected override void context()
+		{
+			_game = new Game();
+			for (int i = 1; i <= 20; i++)
+			{
+				_game.Roll(2);
+			}
+		}
+
+		[Specification]
+		public void the_score_is_40()
+		{
+			_game.Score.ShouldEqual(40);
+		}
+	}
+
 }
---
+++ Source/Bowling/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class Game
+	{
+		private int _score;
+
+		public int Score
+		{
+			get { return _score; }
+		}
+
+		public void Roll(int numberOfPins)
+		{
+			_score += numberOfPins;
+		}
+	}
+}
