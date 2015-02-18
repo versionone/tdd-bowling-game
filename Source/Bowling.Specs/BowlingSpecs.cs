af86c7f1d92396fa2ea5f29ba85e97932d5da734
Josh Gough <jsgough@gmail.com>
2015-02-18 16:24:24 -0500

    Bowling!

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,20 +1,135 @@
+using Bowling;
 using Bowling.Specs.Infrastructure;
+using NUnit.Framework;
 
 namespace specs_for_bowling
 {
 	public class when_everything_is_wired_up : concerns
 	{
-		private bool _itWorked;
+		private Game game;
 
-		protected override void context()
+		[SetUp]
+		public void Setup()
 		{
-			_itWorked = true;
+			game = new Game();
 		}
 
 		[Specification]
-		public void it_works()
-		{
-			_itWorked.ShouldBeTrue();
+		public void when_rolling_all_gutter_balls_the_score_is_0() {
+			20.times(() =>
+			{
+				game.Roll(0);
+			});
+			game.Score().ShouldEqual(0);
 		}
+
+		[Specification]
+		public void when_rolling_all_2s_the_score_is_40()
+		{
+			20.times(() =>
+				{
+					game.Roll(2);
+				});
+			game.Score().ShouldEqual(40);
+		}
+
+		[Specification]
+		public void when_the_first_2_rolls_are_2_and_the_rest_are_3_the_score_is_58()
+		{
+			2.times(() =>
+				{
+					game.Roll(2);
+				});
+			18.times(() =>
+			{
+				game.Roll(3);
+			});
+			game.Score().ShouldEqual(58);
+		}
+
+		[Specification]
+		public void when_rolling_alternating_2s_and_5s_the_score_70()
+		{
+			10.times(() =>
+			{
+				game.Roll(2);
+				game.Roll(5);
+			});
+			game.Score().ShouldEqual(70);
+		}
+
+		[Specification]
+		public void when_the_first_frame_is_a_spare_and_the_remaining_rolls_are_all_2_the_score_is_48()
+		{
+			game.Roll(5);
+			game.Roll(5);
+			18.times(() =>
+			{
+				game.Roll(2);
+			});
+			game.Score().ShouldEqual(48);
+		}
+
+		[Specification]
+		public void when_the_first_2_frames_are_spare_as_2_and_8_and_the_rest_score_2_the_score_is_56()
+		{
+			2.times(() =>
+			{
+				game.Roll(2);
+				game.Roll(8);
+			});
+			16.times(() =>
+			{
+				game.Roll(2);
+			});
+			game.Score().ShouldEqual(56);
+		}
+
+		[Specification]
+		public void when_10_frames_have_been_bowled_do_not_allow_any_more_to_be_bowled()
+		{
+			9.times(() =>
+			{
+				game.Roll(2);
+				game.Roll(8);
+			});
+			game.Roll(2);
+			game.Roll(0);
+			game.Score().ShouldEqual(110);
+			// Game is over after the last roll. But, this guy is nuts and wants to keep rolling!
+			// His score should NOT increase...
+			game.Roll(5);
+			game.Score().ShouldEqual(110);
+		}
+
+		[Specification]
+		public void when_the_first_frame_is_a_strike_and_the_rest_score_2_the_score_is_50()
+		{
+			game.Roll(10);
+			18.times(()=> game.Roll(2));
+			game.Score().ShouldEqual(50);
+		}
+
+		[Specification]
+		public void when_the_first_2_frames_are_strikes_and_the_rest_score_2_the_score_is_68()
+		{
+			game.Roll(10);
+			game.Roll(10);
+			16.times(()=> game.Roll(2));
+			game.Score().ShouldEqual(68);
+		}
+
+		[Specification]
+		public void when_rolling_a_perfect_game_the_score_is_300()
+		{
+			12.times(() => game.Roll(10));
+			game.Score().ShouldEqual(300);
+		}
+
+		/*
+() {
+public void when_rolling_alternate_strikes_and_spares_the_score_is_200() {
+		 */
+
 	}
 }
---
+++ Source/Bowling/Game.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class Game
+	{
+		public Game()
+		{
+		}
+
+		private List<Frame> _frames = new List<Frame>();
+
+		public void Roll(int pins)
+		{			
+			if (_frames.Count == 10 && _frames.Last().Open == false)
+			{
+				return;
+			}
+
+			// first frame
+			if (_frames.Count == 0)
+			{
+				var frame = new Frame(1, pins);
+				_frames.Add(frame);
+			}
+			else
+			{
+				var lastFrame = _frames.Last();
+				if (!lastFrame.Open)
+				{
+					Frame newFrame;
+					if (_frames.Count == 9)
+					{
+						newFrame = new TenthFrame(_frames.Count + 1, pins);
+					}
+					else
+					{
+						newFrame = new Frame(_frames.Count + 1, pins);
+					}
+					_frames.Add(newFrame);
+				}
+				else
+				{
+					lastFrame.Roll(pins);
+				}
+			}
+		}
+
+		public int Score()
+		{
+			return _frames.Sum(
+				(frame) =>
+				{
+					var frameScore = frame.Score();
+					if (frame.IsSpare || frame.IsStrike)
+					{
+						var nextFrame = _frames.Find(fr => fr.FrameIndex == frame.FrameIndex + 1);
+						if (nextFrame != null)
+						{
+							frameScore += nextFrame.FirstRoll.Value;
+						}
+						if (frame.IsStrike)
+						{
+							if (nextFrame.SecondRoll.HasValue)
+							{
+								frameScore += nextFrame.SecondRoll.Value;
+							}
+							else
+							{
+								var secondBonusFrame = _frames.Find(fr => fr.FrameIndex == frame.FrameIndex + 2);
+								if (secondBonusFrame != null)
+								{
+									frameScore += secondBonusFrame.FirstRoll.Value;
+								}
+							}
+
+						}
+					}
+
+					return frameScore;
+				});
+		}
+	}
+
+	public class Frame
+	{
+		public Frame(int frameIndex, int firstRoll)
+		{
+			FirstRoll = firstRoll;
+			FrameIndex = frameIndex;
+		}
+
+		public int FrameIndex { get; set; }
+
+		public int? FirstRoll { get; set; }
+		public int? SecondRoll { get; set; }
+
+		public virtual bool Open
+		{
+			get
+			{
+				return !FirstRoll.HasValue || (
+					!SecondRoll.HasValue && !IsStrike);
+			}
+		}
+
+		public virtual int Score()
+		{
+			return FirstRoll.Value + (SecondRoll.HasValue ? SecondRoll.Value : 0);
+		}
+
+		public virtual void Roll(int pins) {
+			SecondRoll = pins;
+		}
+
+		public bool IsStrike
+		{
+			get
+			{
+				return FirstRoll == 10;
+			}
+		}
+
+		public bool IsSpare
+		{
+			get
+			{
+				return FirstRoll != 10 && FirstRoll + SecondRoll == 10;
+			}
+		}
+	}
+
+	public class TenthFrame : Frame
+	{
+		public TenthFrame(int frameIndex, int pins) : base(frameIndex, pins) {}
+		
+		public int? ThirdRoll { get; set; }
+
+		public override bool Open
+		{
+			get
+			{
+				return (
+					(IsSpare || IsStrike) && (!ThirdRoll.HasValue) ||
+					!SecondRoll.HasValue
+				);
+			}
+		}
+
+		public override int Score() {
+			return FirstRoll.Value + (SecondRoll.HasValue ? SecondRoll.Value : 0)
+				+ (ThirdRoll.HasValue ? ThirdRoll.Value : 0);
+		}
+
+		public override void Roll(int pins) {
+			if (!SecondRoll.HasValue) SecondRoll = pins;
+			else ThirdRoll = pins;
+		}
+	}
+}
