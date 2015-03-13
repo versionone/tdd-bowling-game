64edb3960841cc02370eb49df87934f44d7c9929
rajiv <rajiv.delwadia@versionone.com>
2015-03-13 10:45:29 -0400

    second test green

---
+++ Source/Bowling.Specs/BowlingGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling.Specs
+{
+	class BowlingGame
+	{
+		private int _totalPins;
+
+		internal void Roll(int pins)
+		{
+			_totalPins += pins;
+		}
+
+		internal int GetScore()
+		{
+			return _totalPins;
+		}
+	}
+}
--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -40,4 +40,25 @@ namespace specs_for_bowling
 			score.ShouldEqual(0);
 		}
 	}
+
+	public class when_rolling_all_2s : concerns
+	{
+		private BowlingGame _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new BowlingGame();
+			for (var i = 0; i < 20; i++)
+			{
+				_bowlingGame.Roll(2);
+			}
+		}
+
+		[Specification]
+		public void score_is_40()
+		{
+			var score = _bowlingGame.GetScore();
+			score.ShouldEqual(40);
+		}
+	}
 }
