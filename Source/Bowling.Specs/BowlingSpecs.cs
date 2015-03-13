5ca38f7689fd09ec05a11fbfb395d0a56553a58b
rajiv <rajiv.delwadia@versionone.com>
2015-03-13 10:49:46 -0400

    green test #3

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -45,4 +45,27 @@ namespace specs_for_bowling
 			score.ShouldEqual(40);
 		}
 	}
+
+	public class when_rolling_two_2s_then_all_3s : concerns
+	{
+		private BowlingGame _bowlingGame;
+
+		protected override void context()
+		{
+			_bowlingGame = new BowlingGame();
+			_bowlingGame.Roll(2);
+			_bowlingGame.Roll(2);
+			for (var i = 0; i < 18; i++)
+			{
+				_bowlingGame.Roll(3);
+			}
+		}
+
+		[Specification]
+		public void score_is_58()
+		{
+			var score = _bowlingGame.GetScore();
+			score.ShouldEqual(58);
+		}
+	}
 }
