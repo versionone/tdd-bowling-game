5356ea2f9768cbf19650fa7d43cf972e57da3fca
Keith Gargano <keith.gargano@versionone.com>
2015-06-05 10:15:04 -0400

    spare test defined

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -96,13 +96,32 @@ namespace specs_for_bowling
 		}
 	}
 
+	public class when_The_first_frame_is_a_spare_and_the_remaining_frames_are_twos : concerns
+	{
+		Game game = new Game();
+
+		protected override void context()
+		{
+			game.Roll(5);
+			game.Roll(5);
+			for (int i = 0; i < 18; i++)
+			{
+				game.Roll(2);
+			}
+		}
+
+		[Specification]
+		public void then_the_score_is_48()
+		{
+			game.Score.ShouldEqual(48);
+		}
+	}
+
+
+
 	/*
 	 * 
 	 * 
-* when rolling all gutter balls, the score is 0.
-* when rolling all 2s, the score is 40.
-* when the first 2 rolls are 2 and the rest are 3, the score is 58.
-* when rolling alternating 2s and 5s, the score 70.
 * when the first frame is a spare and the remaining rolls are all 2, the score is 48.
 * when the first 2 frames are spare (as 2,8) and the rest score 2, the score is 56.
 * when 10 frames have been bowled, don't allow any more to be bowled.
