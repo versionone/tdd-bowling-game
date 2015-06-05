35e7bdf0f06cea76ea8e68775a1e9b2cf5d16dc9
Keith Gargano <keith.gargano@versionone.com>
2015-06-05 10:29:39 -0400

    fifth test passed (refactoring)

--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -8,10 +8,18 @@ namespace Bowling
 	public class Game
 	{
 		public int Score { get; private set; }
+		private int LastRoll { get; set; }
+		private int NumberOfRolls { get; set; }
+		private bool PreviousSpare { get; set; }
 
 		public void Roll(int pinsPerRoll)
 		{
-			Score += pinsPerRoll;
+			Score += (PreviousSpare ? pinsPerRoll * 2 : pinsPerRoll);
+			LastRoll = pinsPerRoll;
+
+			if (NumberOfRolls % 2 == 1 && pinsPerRoll + LastRoll == 10) PreviousSpare = true;
+			else PreviousSpare = false;
+			NumberOfRolls++;
 		}
 	}
 }
