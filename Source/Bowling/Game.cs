312ce42bed322eaaaf3d5c4122c68b712187e797
Keith Gargano <keith.gargano@versionone.com>
2015-06-05 09:56:09 -0400

    first test specced

---
+++ Source/Bowling/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class Game
+	{
+		public int Play()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
