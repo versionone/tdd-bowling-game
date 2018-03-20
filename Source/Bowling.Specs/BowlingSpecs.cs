12786257724e8f8c948f6df36c29cfe651c826e5
rajiv <rajiv.delwadia@versionone.com>
2018-03-20 10:58:59 -0400

    Before adding frames

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -14,9 +14,59 @@ namespace Bowling.Specs
 		}
 
 		[Test]
-		public void it_works()
+		public void when_rolling_all_gutter_balls_the_score_is_0()
 		{
-			_itWorked.ShouldBeTrue();
+			var game = new Game();
+			for (var i = 0; i < 20; ++i)
+				game.Roll(0);
+			Assert.AreEqual(0, game.GetScore());
+		}
+
+		[Test]
+		public void when_rolling_all_2s_the_score_is_40()
+		{
+			var game = new Game();
+			for (var i = 0; i < 20; ++i)
+				game.Roll(2);
+			Assert.AreEqual(40, game.GetScore());
+		}
+
+		[Test]
+		public void when_the_first_two_rolls_are_2_and_the_rest_are_3()
+		{
+			var game = new Game();
+			game.Roll(2);
+			game.Roll(2);
+			for (var i = 0; i < 18; ++i)
+				game.Roll(3);
+			Assert.AreEqual(58, game.GetScore());
+		}
+
+		[Test]
+		public void when_rolling_alternating_2s_and_5s()
+		{
+			var game = new Game();
+			for (var i = 0; i < 10; ++i)
+			{
+				game.Roll(2);
+				game.Roll(5);
+			}
+
+			Assert.AreEqual(70, game.GetScore());
+		}
+
+		[Test]
+		public void when_the_first_frame_is_a_spare_and_the_remaining_rolls_are_all_2()
+		{
+			var game = new Game();
+			game.Roll(2);
+			game.Roll(8);
+			for (var i = 0; i < 18; ++i)
+			{
+				game.Roll(2);
+			}
+
+			Assert.AreEqual(48, game.GetScore());
 		}
 	}
 }
--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -1,6 +1,21 @@
-﻿namespace Bowling
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Bowling
 {
 	public class Game
 	{
+		private List<int> rollScores = new List<int>();
+		public void Roll(int pins)
+		{
+			rollScores.Add(pins);
+		}
+		public int GetScore()
+		{
+			int finalScore = 0;
+			for (var i = 0; i < rollScores.Count; i++)
+				finalScore += rollScores[i];
+			return finalScore;
+		}
 	}
 }
