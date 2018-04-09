efda2024aef1cfe7865247a5173449e45c3a5edf
Pavel Mamut <pavel@versionone.com>
2018-04-09 18:30:49 -0400

    introduced Frame concept and moved ball & score tracking responsibilities into it

--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -1,85 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bowling
 {
 	public class Game
 	{
-		private int _score;
-		private int _ballsThrown;
-		private bool _isSpare;
-		private int _lastBallThrown;
-		private int _numberOfFrames = 1;
+		readonly List<Frame> _frames = new List<Frame>();
+
+		public Game()
+		{
+			_frames.Add(new Frame());
+		}
 
 		public int Score
 		{
-			get { return _score; }
+			get
+			{
+				var score = 0;
+				foreach (var frame in _frames)
+				{
+					score += frame.Score;
+				}
+				return score;
+			}
 		}
 
 		public void Roll(int pins)
 		{
 			CheckForGameOver();
 
-			AdvanceToNextBallOfCurrentFrame();
+			TrackRollOfCurrentFrame(pins);
 
-			if (_isSpare)
-			{
-				AddSpareBonus(pins);
-				_isSpare = false;
-			}
+			AddSpareBonus(pins);
 
-			if (HaveCompleteFrame() && CurrentFrameIsSpare(pins))
-				_isSpare = true;
-
-			AddRollToScore(pins);
-
-			if (HaveCompleteFrame())
-			{
+			if (HaveCompleteCurrentFrame())
 				StartNewFrame();
-			}
-			else
-				TrackFirstRollOfCurrentFrame(pins);
 		}
 
-		private int AdvanceToNextBallOfCurrentFrame()
-		{
-			return _ballsThrown += 1;
-		}
 
-		private int AddRollToScore(int pins)
+		private Frame LastFrame
 		{
-			return _score = _score + pins;
+			get { return _frames[_frames.Count - 2]; }
 		}
 
 		private void AddSpareBonus(int pins)
 		{
-			_score += pins;
+			if (_frames.Count > 1)
+				LastFrame.AddBonus(pins);
 		}
 
 		private void CheckForGameOver()
 		{
-			if (_numberOfFrames > 10)
+			if (_frames.Count > 10)
 				throw new TooManyFramesException();
 		}
 
-		private int TrackFirstRollOfCurrentFrame(int pins)
+		private void TrackRollOfCurrentFrame(int pins)
 		{
-			return _lastBallThrown = pins;
+			CurrentFrame.Roll(pins);
 		}
 
 		private void StartNewFrame()
 		{
-			_ballsThrown = 0;
-			_numberOfFrames++;
+			_frames.Add(new Frame());
 		}
 
-		private bool CurrentFrameIsSpare(int pins)
+		private bool HaveCompleteCurrentFrame()
 		{
-			return (pins + _lastBallThrown) == 10;
+			return CurrentFrame.IsComplete;
 		}
 
-		private bool HaveCompleteFrame()
+		private Frame CurrentFrame
 		{
-			return _ballsThrown == 2;
+			get { return _frames[_frames.Count - 1]; }
+		}
+
+		class Frame
+		{
+			private int? _firstRoll;
+			private int? _secondRoll;
+			private int? _bonus;
+
+			public int Score { get { return (_firstRoll ?? 0) + (_secondRoll ?? 0) + (_bonus ?? 0); } }
+
+			public bool IsSpare { get { return _secondRoll.HasValue && (_firstRoll.Value + _secondRoll.Value) == 10; } }
+
+			public bool IsComplete { get { return _secondRoll.HasValue; } }
+
+			public void Roll(int pins)
+			{
+				if (!_firstRoll.HasValue)
+					_firstRoll = pins;
+				else
+					_secondRoll = pins;
+			}
+
+			public void AddBonus(int pins)
+			{
+				if (IsSpare && !_bonus.HasValue)
+					_bonus = pins;
+			}
 		}
 	}
 
