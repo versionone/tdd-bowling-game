886e0e93326ae201ba60624effdccbc2ad5d93f6
Pavel Mamut <pavel@versionone.com>
2018-04-09 15:04:22 -0400

    basic rolls are passing

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -19,4 +19,94 @@ namespace Bowling.Specs
 			_itWorked.ShouldBeTrue();
 		}
 	}
+
+	public class when_rolling_all_gutter_balls
+	{
+		private Game _game;
+
+		[SetUp]
+		public void context()
+		{
+			_game = new Game();
+			for (int i = 0; i < 10; i++)
+			{
+				_game.Roll(0);
+				_game.Roll(0);
+			}
+		}
+
+		[Test]
+		public void the_score_is_0()
+		{
+			_game.Score.ShouldEqual(0);
+		}
+	}
+
+	public class when_rolling_all_2s
+	{
+		private Game _game;
+
+		[SetUp]
+		public void context()
+		{
+			_game = new Game();
+			for (int i = 0; i < 10; i++)
+			{
+				_game.Roll(2);
+				_game.Roll(2);
+			}
+		}
+
+		[Test]
+		public void the_score_is_40()
+		{
+			_game.Score.ShouldEqual(40);
+		}
+	}
+
+	public class when_rolling_firat_2_rolls_are_2_and_rest_are_3
+	{
+		private Game _game;
+
+		[SetUp]
+		public void context()
+		{
+			_game = new Game();
+			_game.Roll(2);
+			_game.Roll(2);
+			for (int i = 0; i < 9; i++)
+			{
+				_game.Roll(3);
+				_game.Roll(3);
+			}
+		}
+
+		[Test]
+		public void the_score_is_58()
+		{
+			_game.Score.ShouldEqual(58);
+		}
+	}
+
+	public class when_rolling_alternating_2s_and_5s
+	{
+		private Game _game;
+
+		[SetUp]
+		public void context()
+		{
+			_game = new Game();
+			for (int i = 0; i < 10; i++)
+			{
+				_game.Roll(2);
+				_game.Roll(5);
+			}
+		}
+
+		[Test]
+		public void the_score_is_70()
+		{
+			_game.Score.ShouldEqual(70);
+		}
+	}
 }
--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -2,5 +2,13 @@
 {
 	public class Game
 	{
+		private int _score;
+
+		public int Score { get { return _score; } }
+
+		public void Roll(int pins)
+		{
+			_score = _score + pins;
+		}
 	}
 }
