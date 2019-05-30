56779deebb5adc41147dd5ea69d18801cb7bb090
Pavel Mamut <pavel@collab.net>
2019-05-30 15:03:55 -0400

    stike!

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -59,4 +59,67 @@ namespace Bowling.Specs
 			_score.ShouldEqual(40);
 		}
 	}
+
+	public class when_rolling_twos_and_threes
+	{
+		private int _score;
+
+		[SetUp]
+		public void context()
+		{
+			var game = new Game();
+			2.times(() => game.Roll(2));
+			18.times(() => game.Roll(3));
+			_score = game.Score;
+		}
+
+		[Test]
+		public void score_is_58()
+		{
+			_score.ShouldEqual(58);
+		}
+	}
+
+	public class when_rolling_alternating_twos_and_fives
+	{
+		private int _score;
+
+		[SetUp]
+		public void context()
+		{
+			var game = new Game();
+			10.times(() =>
+			{
+				game.Roll(2);
+				game.Roll(5);
+			});
+			_score = game.Score;
+		}
+
+		[Test]
+		public void score_is_70()
+		{
+			_score.ShouldEqual(70);
+		}
+	}
+
+	public class when_rolling_a_spare_then_all_2s
+	{
+		private int _score;
+
+		[SetUp]
+		public void context()
+		{
+			var game = new Game();
+			2.times(() => game.Roll(5));
+			18.times(() => game.Roll(2));
+			_score = game.Score;
+		}
+
+		[Test]
+		public void score_is_48()
+		{
+			_score.ShouldEqual(48);
+		}
+	}
 }
--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -1,11 +1,42 @@
-﻿namespace Bowling
+﻿using System;
+using System.Diagnostics;
+
+namespace Bowling
 {
 	public class Game
 	{
 		private int _score;
+		private int? _firstRoll;
+		private int? _secondRoll;
 
 		public void Roll(int pins)
 		{
+			if (_firstRoll.HasValue && _secondRoll.HasValue)
+			{
+				if (_firstRoll + _secondRoll == 10)
+				{
+					_score += pins;
+					_firstRoll = pins;
+					_secondRoll = null;
+				}
+				else
+				{
+					_firstRoll = pins;
+					_secondRoll = null;
+				}
+			}
+			else
+			{
+				if (!_firstRoll.HasValue)
+				{
+					_firstRoll = pins;
+				}
+				else
+				{
+					_secondRoll = pins;
+				}
+			}
+
 			_score += pins;
 		}
 
