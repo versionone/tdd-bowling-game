3f4203627966d19a23712aecc1e1f66a4953c194
Pavel Mamut <pavel@collab.net>
2019-07-11 14:04:01 -0400

    first four specs passing

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -19,4 +19,96 @@ namespace Bowling.Specs
 			_itWorked.ShouldBeTrue();
 		}
 	}
+
+	public class when_rolling_all_gutter_balls
+	{
+		Game _game = new Game();
+
+		[SetUp]
+		public void context()
+		{
+			for (var i = 0; i < 20; i++)
+				_game.Roll(0);
+		}
+
+		[Test]
+		public void the_score_is_zero()
+		{
+			_game.GetScore().ShouldEqual(0);
+		}
+	}
+
+	public class when_rolling_all_twos
+	{
+		Game _game = new Game();
+
+		[SetUp]
+		public void context()
+		{
+			for (var i = 0; i < 20; i++)
+				_game.Roll(2);
+		}
+
+		[Test]
+		public void the_score_is_40()
+		{
+			_game.GetScore().ShouldEqual(40);
+		}
+	}
+
+	public class when_rolling_2_twos_followed_by_all_threes
+	{
+		Game _game = new Game();
+
+		[SetUp]
+		public void context()
+		{
+			for (var i = 0; i < 2; i++)
+				_game.Roll(2);
+			for (var i = 0; i < 18; i++)
+				_game.Roll(3);
+		}
+
+		[Test]
+		public void the_score_is_58()
+		{
+			_game.GetScore().ShouldEqual(58);
+		}
+	}
+	public class when_rolling_alternating_2s_and_5s
+	{
+		Game _game = new Game();
+
+		[SetUp]
+		public void context()
+		{
+			for (var i = 0; i < 10; i++)
+			{
+				_game.Roll(2);
+				_game.Roll(5);
+			}
+/*
+			for (var i = 0; i < 20; i++)
+				_game.Roll(i % 2 == 0 ? 2 : 5);
+*/
+/*
+			for (var i = 0; i < 20; i++)
+				if (i % 2 == 0)
+				{
+					_game.Roll(2);
+				}
+				else
+				{
+					_game.Roll(5);
+				}
+*/
+				
+		}
+
+		[Test]
+		public void the_score_is_70()
+		{
+			_game.GetScore().ShouldEqual(70);
+		}
+	}
 }
--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -2,5 +2,16 @@
 {
 	public class Game
 	{
+		private int _score;
+
+		public void Roll(int pins)
+		{
+			_score += pins;
+		}
+
+		public int GetScore()
+		{
+			return _score;
+		}
 	}
 }
