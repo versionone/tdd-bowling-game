c1c3d64022f71ae36f7a57e5b4eced4e8e8459c4
Kevin Marsh <kmarsh@collab.net>
2020-06-19 16:29:44 -0400

    Kevin running through python bowling with Clay to test it out

--- game.py
+++ game.py
@@ -1,2 +1,42 @@
 class Game:
-	pass
+	def __init__(self):
+		self.score = 0
+		self.frames = [Frame()]
+
+	def roll(self, num):
+		self.frames[-1].update(num)
+		if self.frames[-1].complete:
+			self.frames.append(Frame())
+
+	def tally_score(self):
+		for i in range(0, 10):
+			if self.frames[i].strike:
+				self.score += self.frames[i].score + self.frames[i+1].score
+			elif self.frames[i].spare:
+				self.score += self.frames[i].score + self.frames[i + 1].rolls[0]
+			else:
+				self.score += self.frames[i].score
+		return self.score
+
+
+class Frame:
+	def __init__(self):
+		self.rolls = []
+		self.complete = False
+		self.spare = False
+		self.strike = False
+		self.score = 0
+
+	def update(self, num):
+		"check roll# -> Check if spare -> check if strike -> (mark complete?)"
+		self.rolls.append(num)
+		if len(self.rolls) == 2:
+			self.complete = True
+			if sum(self.rolls) == 10:
+				self.spare = True
+		else:
+			if sum(self.rolls) == 10:
+				self.complete = True
+				self.strike = True
+		self.score = sum(self.rolls)
+
--- test_game.py
+++ test_game.py
@@ -1,9 +1,66 @@
 import pytest
 from game import Game
 
+
 @pytest.fixture
 def game():
 	return Game()
 
+
 def test_game_instance(game):
 	assert game
+
+
+def test_bowl_all_zeros(game):
+	for i in range(20):
+		game.roll(0)
+	assert game.tally_score() == 0
+
+
+def test_bowl_all_twos(game):
+	for i in range(20):
+		game.roll(2)
+	assert game.tally_score() == 40
+
+
+def test_bowl_mostly_threes(game):
+	for i in range(2):
+		game.roll(2)
+	for i in range(18):
+		game.roll(3)
+	assert game.tally_score() == 58
+
+
+def test_bowl_two_then_five(game):
+	for i in range(10):
+		game.roll(2)
+		game.roll(5)
+	assert game.tally_score() == 70
+
+
+def test_first_frame_spare(game):
+	game.roll(1)
+	game.roll(9)
+	for i in range(18):
+		game.roll(2)
+	assert game.tally_score() == 48
+
+
+def test_first_two_frame_spare(game):
+	for i in range(2):
+		game.roll(2)
+		game.roll(8)
+	for i in range(16):
+		game.roll(2)
+	assert game.tally_score() == 56
+
+def test_too_many_frames(game):
+	for i in range(21):
+		game.roll(1)
+	assert game.tally_score() == 20
+
+def test_first_frame_strike(game):
+	game.roll(10)
+	for i in range(18):
+		game.roll(2)
+	assert game.tally_score() == 50
