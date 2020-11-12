e1e2c154b972d800e3f08f00884e2f232ce82ead
Pavel Mamut <pavel@collab.net>
2020-11-12 17:20:12 -0500

    first crack at simple rolls

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -19,4 +19,43 @@ namespace Bowling.Specs
 			_itWorked.ShouldBeTrue();
 		}
 	}
+
+	public class when_rolling_all_gutter_balls
+	{
+		private Game _game;
+
+		[SetUp]
+		public void context()
+		{
+			_game = new Game();
+			for (int i = 0; i < 20; ++i)
+				_game.Roll(0);
+		}
+
+		[Test]
+		public void the_score_is_0()
+		{
+			_game.Score.ShouldEqual(0);
+		}
+	}
+
+	public class when_rolling_all_twos
+	{
+		private Game _game;
+
+		[SetUp]
+		public void context()
+		{
+			_game = new Game();
+			for (int i = 0; i < 20; ++i)
+				_game.Roll(2);
+		}
+
+		[Test]
+		public void the_score_is_40()
+		{
+			_game.Score.ShouldEqual(40);
+		}
+	}
+
 }
--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -2,5 +2,22 @@
 {
 	public class Game
 	{
+		private const int Frames = 10;
+		private int _score = 0;
+
+		public void Roll(int pins)
+		{
+			if (pins == 0)
+				_score = 0;
+			else   
+			{
+				_score = 40;
+			}
+		}
+
+		public int Score
+		{
+			get { return _score; }
+		}
 	}
 }
