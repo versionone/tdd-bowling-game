a159ef1c99b44ddeb24ea8f6c3d80744d6009572
Kevin Marsh <kmarsh@collab.net>
2021-07-01 17:35:13 -0400

    complete bowling game
    -- I forgot to have him commit, which was probably good with the short timefram but :shrug:
    -- total time was around 1 hour 20 minutes

--- game.py
+++ game.py
@@ -1,2 +1,31 @@
 class Game:
-	pass
+
+	def __init__(self):
+		self.myscore = 0
+		self.spare = False
+		self.try_num_arr = []
+
+	def roll(self, roll):
+		self.try_num_arr.append(roll)
+
+
+	def get_score(self):
+		for index in range(len(self.try_num_arr)):
+			ms = self.try_num_arr[index]
+
+			if (index == 0) or ( index == 1):
+				self.myscore += ms
+
+			elif(index == 2):
+				if((self.try_num_arr[0] + self.try_num_arr[1]) >= 10):
+				    self.myscore += ms*2
+				else:
+					self.myscore += ms
+
+			else:
+
+				self.myscore += ms
+
+		return self.myscore
+
+
--- test_game.py
+++ test_game.py
@@ -7,3 +7,40 @@ def game():
 
 def test_game_instance(game):
 	assert game
+
+def test_roll_all_zeros(game):
+	for _ in range(20):
+		game.roll(0)
+	score = game.get_score()
+	assert score == 0
+
+
+def test_roll_all_twos(game):
+	for i in range(20):
+		game.roll(2)
+	assert game.get_score() == 40
+
+def test_roll_score_58(game):
+	for i in range(20):
+		if i < 2:
+			game.roll(2)
+		else:
+			game.roll(3)
+	assert game.get_score() == 58
+
+def test_roll_2_4_alternatives(game):
+	for i in range(20):
+		if (i%2 == 0):
+			game.roll(2)
+		elif (i%2 > 0):
+			game.roll(5)
+	assert game.get_score() == 70
+
+def test_roll_first_frame_spare(game):
+	for i in range(20):
+		if (i < 2):
+			game.roll(5)
+		else:
+			game.roll(2)
+
+	assert game.get_score() == 48
